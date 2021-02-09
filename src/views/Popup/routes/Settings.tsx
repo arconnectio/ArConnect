26de2f45daf76114d2afb80a49e081b57b93a14a
Marton Lederer <marton@lederer.hu>
2021-02-09 13:33:14 +0100

    feat: permission manager

--- src/views/Popup/routes/Settings.tsx
+++ src/views/Popup/routes/Settings.tsx
@@ -1,14 +1,50 @@
-import React, { useState } from "react";
-import { ArrowLeftIcon, ChevronRightIcon } from "@primer/octicons-react";
+import React, { useEffect, useState } from "react";
+import {
+  ArrowLeftIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+  XIcon
+} from "@primer/octicons-react";
 import { goTo } from "react-chrome-extension-router";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../stores/reducers";
 import Home from "./Home";
 import SubPageTopStyles from "../../../styles/components/SubPageTop.module.sass";
 import styles from "../../../styles/views/Popup/settings.module.sass";
+import { removePermissions } from "../../../stores/actions";
 
 export default function Settings() {
   const [setting, setCurrSetting] = useState<
-    "events" | "permissions" | "psts" | "sites"
-  >();
+      "events" | "permissions" | "psts" | "sites"
+    >(),
+    permissions = useSelector((state: RootState) => state.permissions),
+    [opened, setOpened] = useState<{ url: string; opened: boolean }[]>([]),
+    dispatch = useDispatch();
+
+  useEffect(() => {
+    setOpened(permissions.map(({ url }) => ({ url, opened: false })));
+    // eslint-disable-next-line
+  }, []);
+
+  function open(url: string) {
+    setOpened((val) => [
+      ...val.filter((permissionGround) => permissionGround.url !== url),
+      { url, opened: !isOpened(url) }
+    ]);
+  }
+
+  function isOpened(url: string) {
+    return (
+      opened.find((permissionGround) => permissionGround.url === url)?.opened ??
+      false
+    );
+  }
+
+  function filterWithPermission() {
+    return permissions.filter(
+      (permissionGroup) => permissionGroup.permissions.length > 0
+    );
+  }
 
   return (
     <>
@@ -31,7 +67,7 @@ export default function Settings() {
         </h1>
       </div>
       <div className={styles.Settings}>
-        {!setting && (
+        {(!setting && (
           <>
             <div
               className={styles.Setting}
@@ -82,7 +118,45 @@ export default function Settings() {
               </div>
             </div>
           </>
-        )}
+        )) ||
+          (setting === "permissions" && (
+            <>
+              {(filterWithPermission().length > 0 &&
+                filterWithPermission().map((permissionGroup, i) => (
+                  <div key={i}>
+                    <div
+                      className={styles.Setting + " " + styles.SubSetting}
+                      onClick={() => open(permissionGroup.url)}
+                    >
+                      <h1>{permissionGroup.url}</h1>
+                      <div className={styles.Arrow}>
+                        {(isOpened(permissionGroup.url) && (
+                          <ChevronDownIcon />
+                        )) || <ChevronRightIcon />}
+                      </div>
+                    </div>
+                    <div className={styles.OptionContent}>
+                      {isOpened(permissionGroup.url) &&
+                        permissionGroup.permissions.map((perm, j) => (
+                          <h1 key={j}>
+                            {perm}
+                            <div
+                              className={styles.RemoveOption}
+                              onClick={() =>
+                                dispatch(
+                                  removePermissions(permissionGroup.url, [perm])
+                                )
+                              }
+                            >
+                              <XIcon />
+                            </div>
+                          </h1>
+                        ))}
+                    </div>
+                  </div>
+                ))) || <p>No permissions...</p>}
+            </>
+          ))}
       </div>
     </>
   );
