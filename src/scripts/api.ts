9ce5a86602ba1e2b5a7ceccc216bae6bb64a0a82
Marton Lederer <marton@lederer.hu>
2021-02-13 00:37:18 +0100

    feat: signing effect

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -18,6 +18,42 @@ function createOverlay(text: string) {
   return container;
 }
 
+// animation on transaction event
+function createCoinWithAnimation() {
+  const arCoin = document.createElement("img"),
+    pos = { x: 0, y: 0 },
+    id = `ar-coin-animation-${
+      document.querySelectorAll(".ar-coing-animation").length
+    }`;
+  let visibility = 100;
+
+  arCoin.setAttribute("src", "https://www.arweave.org/favicon-183x183.png");
+  arCoin.setAttribute("alt", "a");
+  arCoin.style.position = "fixed";
+  arCoin.style.bottom = "0";
+  arCoin.style.right = `${Math.floor(Math.random() * 30)}px`;
+  arCoin.style.width = "18px";
+  arCoin.style.zIndex = "1000000";
+  arCoin.style.transition = "all .23s ease";
+  arCoin.id = id;
+  arCoin.classList.add("ar-coing-animation");
+  document.body.appendChild(arCoin);
+
+  // @ts-ignore
+  const animation = setInterval(() => {
+    if (visibility < 0) {
+      document.querySelector(`#${id}`)?.remove();
+      return clearInterval(animation);
+    }
+
+    visibility -= 6.5;
+    pos.x += Math.floor(Math.random() * 30) - 10;
+    pos.y += Math.floor(Math.random() * 24);
+    arCoin.style.transform = `translate(-${pos.x}px, -${pos.y}px)`;
+    arCoin.style.opacity = `${visibility / 100}`;
+  }, 100);
+}
+
 const WalletAPI = {
   connect(permissions: PermissionType[]): Promise<void> {
     const requestPermissionOverlay = createOverlay(
@@ -126,6 +162,8 @@ const WalletAPI = {
           const decodeTransaction = arweave.transactions.fromRaw(
             e.data.transaction
           );
+          for (let i = 0; i < 8; i++)
+            setTimeout(() => createCoinWithAnimation(), i * 150);
           resolve(decodeTransaction);
         } else reject(e.data.message);
       }
