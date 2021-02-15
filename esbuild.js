f83fc4c99f7c135487a96a59fcc877396fbac778
Marton Lederer <marton@lederer.hu>
2021-02-15 20:48:26 +0100

    fix: global is undefined

--- esbuild.js
+++ esbuild.js
@@ -22,7 +22,8 @@ esbuild
     target: ["chrome58", "firefox57"],
     outdir: "./public/build",
     define: {
-      "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`
+      "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+      global: "globalThis"
     },
     loader: {
       ".png": "dataurl"
