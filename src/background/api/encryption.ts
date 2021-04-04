476844567b2b6081cd74152c98c540ee4c097cbd
Marton Lederer <marton@lederer.hu>
2021-04-04 18:26:02 +0200

    feat: decrypt

--- src/background/api/encryption.ts
+++ src/background/api/encryption.ts
@@ -53,7 +53,7 @@ export const decrypt = (message: MessageFormat, tabURL: string) =>
 
       resolve({
         res: true,
-        data: await doDecrypt(message), // TODO
+        data: await doDecrypt(message),
         message: "Success"
       });
     } catch {
@@ -119,4 +119,60 @@ async function doEncrypt(message: MessageFormat) {
     );
 
   return arweave.utils.concatBuffers([encryptedKey, encryptedData]);
+}
+
+async function doDecrypt(message: MessageFormat) {
+  const arweave = new Arweave({
+      host: "arweave.net",
+      port: 443,
+      protocol: "https"
+    }),
+    storeData = await getStoreData(),
+    storedKeyfiles = storeData?.["wallets"],
+    storedAddress = storeData?.["profile"],
+    keyfileToDecrypt = storedKeyfiles?.find(
+      (item) => item.address === storedAddress
+    )?.keyfile;
+
+  if (!storedKeyfiles || !storedAddress || !keyfileToDecrypt)
+    throw new Error("No wallets added");
+
+  const keyfile: JWKInterface = JSON.parse(atob(keyfileToDecrypt)),
+    obj = {
+      ...keyfile,
+      alg: "RSA-OAEP-256",
+      ext: true
+    },
+    key = await crypto.subtle.importKey(
+      "jwk",
+      obj,
+      {
+        name: message.options.algorithm,
+        hash: {
+          name: message.options.hash
+        }
+      },
+      false,
+      ["decrypt"]
+    );
+
+  const encryptedKey = new Uint8Array(
+      new Uint8Array(Object.values(message.data)).slice(0, 512)
+    ),
+    encryptedData = new Uint8Array(
+      new Uint8Array(Object.values(message.data)).slice(512)
+    );
+
+  const symmetricKey = await crypto.subtle.decrypt(
+    { name: message.options.algorithm },
+    key,
+    encryptedKey
+  );
+
+  const res = await arweave.crypto.decrypt(
+    encryptedData,
+    new Uint8Array(symmetricKey)
+  );
+
+  return arweave.utils.bufferToString(res).split(message.options.salt)[0];
 }
