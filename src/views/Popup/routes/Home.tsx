e09286977b6a70654cd72f103fb8ba9592356dad
Marton Lederer <marton@lederer.hu>
2021-07-12 21:22:02 +0200

    chore: war logo

--- src/views/Popup/routes/Home.tsx
+++ src/views/Popup/routes/Home.tsx
@@ -36,7 +36,7 @@ import Verto from "@verto/lib";
 import arweaveLogo from "../../../assets/arweave.png";
 import verto_light_logo from "../../../assets/verto_light.png";
 import verto_dark_logo from "../../../assets/verto_dark.png";
-import WARLogo from "../../../assets/arweave.png";
+import WARLogo from "../../../assets/war.png";
 import styles from "../../../styles/views/Popup/home.module.sass";
 
 export default function Home() {
