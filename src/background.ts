a49d3fdc61fe30ef770e5b92e4bb0916beff7aa3
Marton Lederer <marton@lederer.hu>
2022-09-13 18:42:32 +0200

    chore: move address change listener

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,7 @@
-import { onMessage, sendMessage } from "webext-bridge";
+import { addressChangeListener } from "~wallets/event";
 import { getStorageConfig } from "~utils/storage";
 import { Storage } from "@plasmohq/storage";
-import { getAppURL } from "~utils/format";
-import Application from "~applications/application";
-import browser from "webextension-polyfill";
+import { onMessage } from "webext-bridge";
 
 const storage = new Storage(getStorageConfig());
 
@@ -21,36 +19,6 @@ onMessage("api_call", async ({ data }) => {
 // watch for active address changes
 // and send them to the content script to
 // fire the wallet switch event
-storage.watch({
-  async active_address({ newValue: newAddress }) {
-    if (!newAddress) return;
-
-    // get all tabs
-    const tabs = await browser.tabs.query({});
-
-    // go through all tabs and check if they
-    // have the permissions to receive the
-    // wallet switch event
-    for (const tab of tabs) {
-      if (!tab.id || !tab.url) continue;
-
-      const app = new Application(getAppURL(tab.url));
-
-      // check required permissions
-      if (
-        !(await app.hasPermissions(["ACCESS_ALL_ADDRESSES", "ACCESS_ADDRESS"]))
-      ) {
-        continue;
-      }
-
-      // trigger event via message
-      await sendMessage(
-        "switch_wallet_event",
-        newAddress,
-        `content-script@${tab.id}`
-      );
-    }
-  }
-});
+storage.watch({ active_address: addressChangeListener });
 
 export {};
---
+++ src/wallets/event.ts
@@ -0,0 +1,41 @@
+import Application from "~applications/application";
+import browser, { Storage } from "webextension-polyfill";
+import { sendMessage } from "webext-bridge";
+import { getAppURL } from "~utils/format";
+
+/**
+ * Active address change event listener.
+ * Sends a message to fire the "walletSwitch"
+ * event in the tab.
+ */
+export async function addressChangeListener({
+  newValue: newAddress
+}: Storage.StorageChange) {
+  if (!newAddress) return;
+
+  // get all tabs
+  const tabs = await browser.tabs.query({});
+
+  // go through all tabs and check if they
+  // have the permissions to receive the
+  // wallet switch event
+  for (const tab of tabs) {
+    if (!tab.id || !tab.url) continue;
+
+    const app = new Application(getAppURL(tab.url));
+
+    // check required permissions
+    if (
+      !(await app.hasPermissions(["ACCESS_ALL_ADDRESSES", "ACCESS_ADDRESS"]))
+    ) {
+      continue;
+    }
+
+    // trigger event via message
+    await sendMessage(
+      "switch_wallet_event",
+      newAddress,
+      `content-script@${tab.id}`
+    );
+  }
+}
