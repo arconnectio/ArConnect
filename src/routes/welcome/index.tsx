d6b957fe6a8c4832a4fd380288df489b6f0ab9cf
Marton Lederer <marton@lederer.hu>
2022-10-22 18:01:22 +0200

    feat: welcome animation

--- src/routes/welcome/index.tsx
+++ src/routes/welcome/index.tsx
@@ -1,9 +1,41 @@
 import { Button, Spacer, Text } from "@arconnect/components";
-import browser from "webextension-polyfill";
-import styled, { keyframes } from "styled-components";
 import { ArrowRightIcon, KeyIcon } from "@iconicicons/react";
+import { AnimatePresence, motion } from "framer-motion";
+import styled, { keyframes } from "styled-components";
+import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
+import browser from "webextension-polyfill";
 
 export default function Home() {
+  // button refs
+  const startButton = useRef<HTMLButtonElement>();
+  const walletButton = useRef<HTMLButtonElement>();
+
+  // position of the expand animation element
+  const [expandPos, setExpandPos] = useState<{ x: number; y: number }>();
+
+  // expand animation functionality
+  function animate(btn: MutableRefObject<HTMLButtonElement>) {
+    // get pos
+    const btnDimensions = btn.current.getBoundingClientRect();
+
+    setExpandPos({
+      x: btnDimensions.x + btnDimensions.width / 2,
+      y: btnDimensions.y + btnDimensions.height / 2
+    });
+  }
+
+  // window size
+  const windowDimensions = useWindowDimensions();
+
+  // circle size
+  const circleSize = useMemo(
+    () =>
+      windowDimensions.height > windowDimensions.width
+        ? windowDimensions.height
+        : windowDimensions.width,
+    [windowDimensions]
+  );
+
   return (
     <Wrapper>
       <Panel>
@@ -14,11 +46,18 @@ export default function Home() {
           </RotatingName>
           <Spacer y={1.35} />
           <ButtonsWrapper>
-            <WelcomeButton>
+            <WelcomeButton
+              ref={startButton}
+              onClick={() => animate(startButton)}
+            >
               {browser.i18n.getMessage("get_me_started")}
               <ArrowRightIcon />
             </WelcomeButton>
-            <WelcomeButton secondary>
+            <WelcomeButton
+              secondary
+              ref={walletButton}
+              onClick={() => animate(walletButton)}
+            >
               {browser.i18n.getMessage("have_wallet")}
               <KeyIcon />
             </WelcomeButton>
@@ -26,11 +65,17 @@ export default function Home() {
         </WelcomeContent>
       </Panel>
       <EcosystemPanel></EcosystemPanel>
+      <AnimatePresence>
+        {expandPos && (
+          <ExpandAnimationElement pos={expandPos} circleSize={circleSize} />
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
+  position: relative;
   display: flex;
   align-items: stretch;
   width: 100vw;
@@ -113,3 +158,64 @@ const WelcomeButton = styled(Button)`
   padding-right: 0;
   width: calc(100% - 0.75rem * 1);
 `;
+
+const ExpandAnimationElement = styled(motion.div).attrs<{
+  pos: { x: number; y: number };
+  circleSize: number;
+}>((props) => ({
+  variants: {
+    closed: {
+      opacity: 0,
+      clipPath: `circle(20px at ${props.pos.x + "px"} ${props.pos.y + "px"})`,
+      transition: {
+        type: "easeInOut",
+        duration: 0.75
+      }
+    },
+    open: {
+      opacity: 1,
+      clipPath: `circle(${props.circleSize + "px"} at ${props.pos.x + "px"} ${
+        props.pos.y + "px"
+      })`,
+      transition: {
+        type: "easeInOut",
+        duration: 0.75
+      }
+    }
+  },
+  initial: "closed",
+  animate: "open",
+  exit: "closed"
+}))<{ pos: { x: number; y: number }; circleSize: number }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgb(${(props) => props.theme.theme});
+`;
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    const lisetener = () => setWindowDimensions(getWindowDimensions());
+
+    window.addEventListener("resize", lisetener);
+
+    return () => window.removeEventListener("resize", lisetener);
+  }, []);
+
+  return windowDimensions;
+}
