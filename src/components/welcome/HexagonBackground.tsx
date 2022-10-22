37f2b955911cb66e41f4ab6ff11f5d86b55823ea
Marton Lederer <marton@lederer.hu>
2022-10-22 23:08:49 +0200

    feat: arweave hexagon bg

---
+++ src/components/welcome/HexagonBackground.tsx
@@ -0,0 +1,63 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+// inspired by arweave.org
+export default function HexagonBackground({ images }: Props) {
+  return (
+    <Wrapper>
+      {images.map((src, i) => (
+        <Hexagon
+          src={src}
+          key={i}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.8 - Math.floor(i / 10) * 0.2 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.23, ease: "easeInOut" }}
+          data-test={Math.floor(i / 10)}
+        />
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: absolute;
+  left: -5%;
+  right: -5%;
+  bottom: -5%;
+  top: -5%;
+  width: 110%;
+  height: 110%;
+  z-index: -10;
+  overflow: hidden;
+`;
+
+const Hexagon = styled(motion.div)<{ src: string }>`
+  display: list-item;
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+  float: left;
+  width: 10%;
+  padding-top: 10%;
+  margin: 7px;
+
+  &:nth-child(18n + 10),
+  &:nth-child(18n + 11),
+  &:nth-child(18n + 12),
+  &:nth-child(18n + 13),
+  &:nth-child(18n + 14),
+  &:nth-child(18n + 15),
+  &:nth-child(18n + 16),
+  &:nth-child(18n + 17),
+  &:nth-child(18n + 18) {
+    margin-top: -2.5%;
+    margin-bottom: -2.5%;
+    transform: translateX(calc(50% + 7px));
+  }
+`;
+
+interface Props {
+  images: string[];
+}
--- src/routes/welcome/start.tsx
+++ src/routes/welcome/start.tsx
@@ -1,34 +1,82 @@
 import { Button, Spacer, Text } from "@arconnect/components";
 import { ArrowRightIcon } from "@iconicicons/react";
 import { motion } from "framer-motion";
+import { useLocation } from "wouter";
+import { useState } from "react";
+import HexagonBackground from "~components/welcome/HexagonBackground";
+import browser from "webextension-polyfill";
 import styled from "styled-components";
 
 export default function Start() {
+  // router
+  const [, setLocation] = useLocation();
+
+  // page of the setup
+  const [page, setPage] = useState(1);
+
   return (
     <Wrapper>
+      <Skip onClick={() => setLocation("/generate")}>
+        {browser.i18n.getMessage("skip")}
+      </Skip>
       <ExplainerSection>
-        <ExplainTitle>This is an example</ExplainTitle>
+        <ExplainTitle>
+          {browser.i18n.getMessage(pages.find((_, i) => i === page - 1).title)}
+        </ExplainTitle>
         <Spacer y={0.5} />
-        <Text noMargin>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias,
-          perferendis, alias repudiandae tempora facere, quae laborum eligendi
-          natus quibusdam aut beatae? Est accusamus molestias assumenda corporis
-          ducimus. Omnis, laudantium optio.
-        </Text>
+        <ExplainerContent>
+          {browser.i18n.getMessage(
+            pages.find((_, i) => i === page - 1).content
+          )}
+        </ExplainerContent>
         <Spacer y={1.25} />
-        <Button fullWidth>
-          Next
+        <Button
+          fullWidth
+          onClick={() => {
+            if (page === 3) return setLocation("/generate");
+            setPage((val) => val + 1);
+          }}
+        >
+          {browser.i18n.getMessage("next")}
           <ArrowRightIcon />
         </Button>
       </ExplainerSection>
       <Pagination>
-        <Page />
-        <Page active />
-        <Page />
+        {Array(3)
+          .fill("")
+          .map((_, i) => (
+            <Page
+              onClick={() => setPage(i + 1)}
+              key={i}
+              active={page === i + 1}
+            />
+          ))}
       </Pagination>
+      {page === 1 && (
+        <HexagonBackground
+          images={Array(25).fill(
+            "https://www.arweave.org/images/hexagonbg/1.jpg"
+          )}
+        />
+      )}
     </Wrapper>
   );
 }
+
+const pages: Page[] = [
+  {
+    title: "what_is_arweave",
+    content: "about_arweave"
+  },
+  {
+    title: "what_is_a_permaweb_app",
+    content: "about_permaweb_apps"
+  },
+  {
+    title: "what_is_arconnect",
+    content: "about_arconnect"
+  }
+];
 
 const Wrapper = styled(motion.div).attrs({
   initial: { opacity: 0 },
@@ -37,13 +85,14 @@ const Wrapper = styled(motion.div).attrs({
   position: relative;
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
 `;
 
 const ExplainerSection = styled.div`
   position: absolute;
   left: 3rem;
   bottom: 3rem;
-  width: 30%;
+  width: 31%;
 `;
 
 const ExplainTitle = styled(Text).attrs({
@@ -52,6 +101,12 @@ const ExplainTitle = styled(Text).attrs({
 })`
   font-size: 2.7rem;
   font-weight: 600;
+`;
+
+const ExplainerContent = styled(Text).attrs({
+  noMargin: true
+})`
+  text-align: justify;
 `;
 
 const Pagination = styled.div`
@@ -70,4 +125,27 @@ const Page = styled.span<{ active?: boolean }>`
   background-color: rgb(
     ${(props) => props.theme.theme + ", " + (props.active ? "1" : ".45")}
   );
+  transition: all 0.23s ease-in-out;
 `;
+
+const Skip = styled(Text)`
+  position: absolute;
+  top: 3rem;
+  right: 3rem;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const ArweaveContentWrapper = styled.div`
+  top: 0;
+  right: 0;
+  width: 55%;
+  height: 70%;
+`;
+
+interface Page {
+  // i18n key
+  title: string;
+  // i18n key
+  content: string;
+}
