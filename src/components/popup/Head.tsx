d0576a81bec3abe6db60edb2ebf804c77cf46fcb
Marton Lederer <marton@lederer.hu>
2022-10-24 11:27:41 +0200

    feat: head animation

--- src/components/popup/Head.tsx
+++ src/components/popup/Head.tsx
@@ -6,7 +6,8 @@ import { useStorage } from "@plasmohq/storage/hook";
 import { ArrowLeftIcon } from "@iconicicons/react";
 import type { AnsUser } from "~lib/ans";
 import { useTheme } from "~utils/theme";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { motion } from "framer-motion";
 
 export default function Head({ title }: Props) {
   // scroll position
@@ -40,12 +41,15 @@ export default function Head({ title }: Props) {
   }, [ans]);
 
   return (
-    <HeadWrapper displayTheme={theme} scrolled={scrollY > 14}>
-      <BackButton onClick={() => history.back()} />
-      <PageInfo>
-        <Text subtitle noMargin>
-          {title}
-        </Text>
+    <HeadWrapper displayTheme={theme} scrolled={scrollY > 85}>
+      <BackWrapper>
+        <BackButton onClick={() => history.back()} />
+      </BackWrapper>
+      <PageInfo
+        key={scrollY > 85 ? 0 : 1}
+        scrollDirection={scrollY > 85 ? "down" : "up"}
+      >
+        <PageTitle>{title}</PageTitle>
         <Avatar img={avatar}>{!avatar && <NoAvatarIcon />}</Avatar>
       </PageInfo>
     </HeadWrapper>
@@ -60,13 +64,13 @@ const HeadWrapper = styled(Section)<{
   display: flex;
   align-items: ${(props) => (props.scrolled ? "flex-start" : "center")};
   flex-direction: ${(props) => (props.scrolled ? "column" : "row")};
-  gap: 0.9rem;
+  gap: ${(props) => (props.scrolled ? "0.5rem" : "0.77rem")};
   top: 0;
   left: 0;
   right: 0;
   z-index: 1000;
-  padding-top: 2.2rem;
-  padding-bottom: 1.5rem;
+  padding-top: 2rem;
+  padding-bottom: 0.8rem;
   background-color: rgba(${(props) => props.theme.background}, 0.75);
   backdrop-filter: blur(15px);
   border-bottom: 1px solid;
@@ -76,27 +80,56 @@ const HeadWrapper = styled(Section)<{
         (props.displayTheme === "light" ? "235, 235, 241" : "31, 30, 47") +
         ")"
       : "transparent"};
-  transition: all 0.23s ease-in-out;
+  transition: border-color 0.23s ease-in-out;
+`;
+
+const BackWrapper = styled.div`
+  display: flex;
+  overflow: hidden;
+  width: max-content;
+  height: max-content;
 `;
 
 const BackButton = styled(ArrowLeftIcon)`
-  font-size: 1.45rem;
+  font-size: 1.6rem;
   width: 1em;
   height: 1em;
   color: rgb(${(props) => props.theme.primaryText});
   cursor: pointer;
+  transform: translateX(0);
+  opacity: 1;
   transition: all 0.23s ease-in-out;
 
+  path {
+    stroke-width: 1.75 !important;
+  }
+
   &:hover {
-    opacity: 0.8;
+    opacity: 0.7;
   }
 `;
 
-const PageInfo = styled.div`
+const PageInfo = styled(motion.div).attrs<{
+  scrollDirection: "up" | "down";
+}>((props) => ({
+  initial: { opacity: 0, y: props.scrollDirection === "up" ? 20 : -20 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: props.scrollDirection === "up" ? -20 : 20 }
+}))<{
+  scrollDirection: "up" | "down";
+}>`
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100%;
+`;
+
+const PageTitle = styled(Text).attrs({
+  subtitle: true,
+  noMargin: true
+})`
+  font-size: 1.5rem;
+  font-weight: 500;
 `;
 
 interface Props {
---
+++ src/routes/auth/index.tsx
@@ -0,0 +1,272 @@
+import Arweave from "arweave/web/common";
+import { useEffect, useState } from "react";
+import type { AuthResult } from "shim";
+import { sendMessage } from "@arconnect/webext-bridge";
+import type { AuthDataWithID, AuthType } from "~api/modules/connect/auth";
+import { objectFromUrlParams } from "~api/modules/connect/url";
+import { addApp } from "~applications";
+import Application from "~applications/application";
+import { defaultGateway } from "~applications/gateway";
+import { permissionData, PermissionType } from "~applications/permissions";
+import { unlock } from "~wallets/auth";
+import browser from "webextension-polyfill";
+import Head from "~components/popup/Head";
+
+export default function Home() {
+  // fetch data sent to process with the auth
+  const [initParmas, setInitParams] = useState<AuthDataWithID>();
+
+  useEffect(() => {
+    const urlParams = window.location.href.split("?");
+    const params = objectFromUrlParams<AuthDataWithID>(
+      urlParams[urlParams.length - 1]
+    );
+
+    setInitParams(params);
+  }, []);
+
+  /**
+   * Send the result as a response to the auth
+   *
+   * @param errorMessage Optional error message. If defined, the auth will fail with this message
+   * @param type Optional type of the auth. If not defined, it uses the one defined in "initParams"
+   * @param authID Optional ID of the auth. If not defined, it uses the one defined in "initParams"
+   */
+  async function sendResponse(
+    errorMessage?: string,
+    type?: AuthType,
+    authID?: string
+  ) {
+    const response: AuthResult = {
+      type: type || initParmas.type,
+      authID: authID || initParmas.authID,
+      error: !!errorMessage,
+      data: errorMessage
+    };
+
+    // send the response message
+    await sendMessage("auth_result", response, "background");
+
+    closeWindow();
+  }
+
+  /**
+   * Cancel event (window close, etc.)
+   */
+  const cancel = () => {
+    sendResponse("User cancelled the auth");
+  };
+
+  // send cancel event if the popup is closed by the user
+  window.onbeforeunload = cancel;
+
+  // remove cancel event and close the window
+  function closeWindow() {
+    window.onbeforeunload = null;
+    window.close();
+  }
+
+  const [password, setPassword] = useState<string>();
+  const [authenticated, setAuthenticated] = useState(false);
+
+  // authenticate the user
+  async function auth() {
+    const res = await unlock(password);
+
+    setAuthenticated(res);
+
+    if (!res) {
+      return console.log("Invalid password");
+    }
+
+    if (initParmas.type === "unlock") {
+      await sendResponse();
+    } else if (initParmas.type === "connect") {
+      await addApp({
+        url: initParmas.url,
+        permissions: [],
+        name: initParmas.appInfo?.name,
+        logo: initParmas.appInfo?.logo,
+        gateway: initParmas.gateway || defaultGateway
+      });
+    }
+  }
+
+  if (!initParmas) return <>Loading...</>;
+
+  if (!authenticated) {
+    return (
+      <>
+        <Head title="Auth" />
+        <h2>Auth - {initParmas.type}</h2>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{ width: "100%", display: "block" }}
+        />
+        <button
+          onClick={auth}
+          style={{
+            display: "block",
+            width: "100%",
+            textAlign: "center",
+            paddingLeft: 0,
+            paddingRight: 0
+          }}
+        >
+          {(initParmas.type === "connect" && "Connect") || "Sign In"}
+        </button>
+        <button
+          onClick={cancel}
+          style={{
+            display: "block",
+            width: "100%",
+            textAlign: "center",
+            paddingLeft: 0,
+            paddingRight: 0
+          }}
+        >
+          Cancel
+        </button>
+        <div style={{ height: "800px" }}></div>
+      </>
+    );
+  }
+
+  if (initParmas.type === "allowance") {
+    return (
+      <>
+        <AllowanceManager app={new Application(initParmas.url)} />
+        <button
+          onClick={() => sendResponse()}
+          style={{
+            display: "block",
+            width: "100%",
+            textAlign: "center",
+            paddingLeft: 0,
+            paddingRight: 0
+          }}
+        >
+          Ok
+        </button>
+      </>
+    );
+  }
+
+  if (initParmas.type === "connect") {
+    return (
+      <>
+        <h2>Allow these permissions</h2>
+        <PermissionsManager
+          app={new Application(initParmas.url)}
+          callback={sendResponse}
+          requestedPermissions={initParmas.permissions}
+        />
+      </>
+    );
+  }
+
+  return <></>;
+}
+
+const AllowanceManager = ({ app }: { app: Application }) => {
+  const [settings, updateSettings] = app.hook();
+  const arweave = new Arweave(defaultGateway);
+
+  return (
+    <>
+      <h2>Update your allowance</h2>
+      <label>
+        Limit
+        <input
+          type="number"
+          value={arweave.ar.winstonToAr(settings.allowance.limit.toString())}
+          onChange={(e) =>
+            updateSettings({
+              ...settings,
+              allowance: {
+                ...settings.allowance,
+                limit: parseInt(arweave.ar.arToWinston(e.target.value))
+              }
+            })
+          }
+          style={{ width: "100%", display: "block" }}
+        />
+      </label>
+      <button
+        onClick={() =>
+          updateSettings({
+            ...settings,
+            allowance: {
+              ...settings.allowance,
+              spent: 0
+            }
+          })
+        }
+      >
+        Reset spent AR
+      </button>
+      <br />
+    </>
+  );
+};
+
+const PermissionsManager = ({
+  app,
+  requestedPermissions,
+  callback
+}: {
+  app: Application;
+  requestedPermissions: PermissionType[];
+  callback: any;
+}) => {
+  const [permissions, setPermissions] = useState(requestedPermissions);
+
+  async function ok() {
+    await app.updateSettings({ permissions });
+    await callback();
+  }
+
+  return (
+    <>
+      {requestedPermissions.map((permission, i) => (
+        <div key={i}>
+          <label>
+            <input
+              type="checkbox"
+              defaultChecked
+              onChange={(e) => {
+                if (
+                  e.target.checked &&
+                  !permissions.includes(permission as PermissionType)
+                ) {
+                  setPermissions(
+                    (val) => [...val, permission] as PermissionType[]
+                  );
+                } else if (!e.target.checked) {
+                  setPermissions((val) =>
+                    val.filter((perm) => perm !== permission)
+                  );
+                }
+              }}
+            />
+            {" " + browser.i18n.getMessage(permissionData[permission])}
+          </label>
+        </div>
+      ))}
+      <button
+        onClick={ok}
+        style={{
+          display: "block",
+          width: "100%",
+          textAlign: "center",
+          paddingLeft: 0,
+          paddingRight: 0
+        }}
+      >
+        Ok
+      </button>
+    </>
+  );
+};
