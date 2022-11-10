2c8df9519ca150ad582478477e94fa43051c8dfe
Marton Lederer <marton@lederer.hu>
2022-11-10 09:00:14 +0100

    feat: arns parser

---
+++ src/lib/arweave_news.ts
@@ -0,0 +1,27 @@
+import Parser from "rss-parser";
+
+/**
+ * Fetch the RSS feed of arweave.news
+ */
+export default async function getArweaveNewsFeed() {
+  const parser = new Parser<ArweaveNewsFeed[]>();
+  const feed = await parser.parseURL("https://arweave.news/feed");
+
+  return feed;
+}
+
+export interface ArweaveNewsFeed {
+  creator: string;
+  title: string;
+  link: string;
+  pubDate: string;
+  "content:encoded": string;
+  "content:encodedSnippet": string;
+  "dc:creator": string;
+  comments: string;
+  content: string;
+  contentSnippet: string;
+  guid: string;
+  categories: string[];
+  isoDate: string;
+}
--- src/routes/popup/explore.tsx
+++ src/routes/popup/explore.tsx
@@ -1,14 +1,17 @@
+import { Card, Section, Spacer, Text } from "@arconnect/components";
 import { getMarketChart, getArPrice } from "~lib/coingecko";
 import { useEffect, useState } from "react";
+import getArweaveNewsFeed, { ArweaveNewsFeed } from "~lib/arweave_news";
 import PeriodPicker from "~components/popup/asset/PeriodPicker";
 import PriceChart from "~components/popup/asset/PriceChart";
 import browser from "webextension-polyfill";
 import Head from "~components/popup/Head";
 import useSetting from "~settings/hook";
+import styled from "styled-components";
 
 export default function Explore() {
   // ar price period
-  const [period, setPeriod] = useState("All");
+  const [period, setPeriod] = useState("Day");
 
   // currency setting
   const [currency] = useSetting<string>("currency");
@@ -42,6 +45,18 @@ export default function Explore() {
     })();
   }, [currency]);
 
+  // active featured news page
+  const [featuredPage, setFeaturedPage] = useState<number>(0);
+
+  // parse arweave.news RSS
+  const [feed, setFeed] = useState<ArweaveNewsFeed[]>();
+
+  useEffect(() => {
+    getArweaveNewsFeed()
+      .then((res) => setFeed(res))
+      .catch();
+  }, []);
+
   return (
     <>
       <Head title={browser.i18n.getMessage("explore")} />
@@ -55,6 +70,48 @@ export default function Explore() {
       >
         <PeriodPicker period={period} onChange={(p) => setPeriod(p)} />
       </PriceChart>
+      <Section>
+        <Text heading noMargin>
+          {browser.i18n.getMessage("assets")}
+        </Text>
+        <Spacer y={0.75} />
+        <FeaturedArticles>
+          <Paginator>
+            {new Array(3).fill("").map((_, i) => (
+              <Page
+                active={i === featuredPage}
+                onClick={() => setFeaturedPage(i)}
+              />
+            ))}
+          </Paginator>
+        </FeaturedArticles>
+      </Section>
     </>
   );
 }
+
+const FeaturedArticles = styled(Card)`
+  position: relative;
+  background-color: #000;
+`;
+
+const Paginator = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.42rem;
+  bottom: 0.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+const Page = styled.div<{ active?: boolean }>`
+  width: 4px;
+  height: 4px;
+  border-radius: 100%;
+  border: 1px solid #fff;
+  background-color: ${(props) => (props.active ? "#fff" : "transparent")};
+  cursor: pointer;
+  transition: all 0.23s ease-in-out;
+`;
