23654aac3a5706a957266ac4a332cc85b0c6151f
Marton Lederer <marton@lederer.hu>
2022-11-22 10:44:44 +0100

    feat: sandbox cache wip

---
+++ src/tokens/SandboxCache.ts
@@ -0,0 +1,91 @@
+import type {
+  CacheKey,
+  SortKeyCache,
+  SortKeyCacheResult
+} from "warp-contracts/lib/types/cache/SortKeyCache";
+import type { CacheOptions } from "warp-contracts";
+import { nanoid } from "nanoid";
+
+export default class SandboxCache<V = any> implements SortKeyCache<V> {
+  constructor(cacheOptions: CacheOptions) {}
+
+  async get(contractTxId: string, sortKey: string, returnDeepCopy?: boolean) {
+    return await this.invokeFunction<SortKeyCacheResult<V> | null>("get", [
+      contractTxId,
+      sortKey,
+      returnDeepCopy
+    ]);
+  }
+
+  async put(stateCacheKey: CacheKey, value: V) {
+    return await this.invokeFunction<void>("put", [stateCacheKey, value]);
+  }
+
+  async getLast(contractTxId: string) {
+    return await this.invokeFunction<SortKeyCacheResult<V> | null>("getLast", [
+      contractTxId
+    ]);
+  }
+
+  async getLessOrEqual(contractTxId: string, sortKey: string) {
+    return await this.invokeFunction<SortKeyCacheResult<V> | null>(
+      "getLessOrEqual",
+      [contractTxId, sortKey]
+    );
+  }
+
+  async allContracts() {
+    return await this.invokeFunction<string[]>("allContracts");
+  }
+
+  async close() {
+    return await this.invokeFunction<void>("close");
+  }
+
+  async dump(): Promise<any> {
+    throw new Error("Not implemented yet");
+  }
+
+  async getLastSortKey(): Promise<string | null> {
+    throw new Error("Not implemented yet");
+  }
+
+  async invokeFunction<T = any>(fn: string, params: any[] = []) {
+    // generate an ID for the call
+    const callID = nanoid();
+
+    return await new Promise<T>((resolve, reject) => {
+      // listen for the result
+      const resultListener = (
+        e: MessageEvent<{
+          callID: string;
+          error?: boolean;
+          res: T | string;
+        }>
+      ) => {
+        // check message ID
+        if (e.data.callID !== callID) return;
+
+        // remove listener
+        window.removeEventListener("message", resultListener);
+
+        // handle result
+        if (e.data.error) {
+          reject(e.data.res as string);
+        } else {
+          resolve(e.data.res as T);
+        }
+      };
+
+      // add listener
+      window.addEventListener("message", resultListener);
+
+      // send call message
+      window.postMessage({
+        fn,
+        callID,
+        params
+      });
+    });
+  }
+}
