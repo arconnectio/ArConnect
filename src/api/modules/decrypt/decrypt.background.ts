767441dffed8042d5358b809f3bf1d3fd34e78ca
Marton Lederer <marton@lederer.hu>
2022-11-25 18:14:55 +0100

    chore: update encrypt/decrypt

--- src/api/modules/decrypt/decrypt.background.ts
+++ src/api/modules/decrypt/decrypt.background.ts
@@ -4,14 +4,10 @@ import { getActiveKeyfile } from "~wallets";
 import browser from "webextension-polyfill";
 import Arweave from "@arconnect/arweave";
 
-const background: ModuleFunction<string> = async (
+const background: ModuleFunction<string | Uint8Array> = async (
   _,
-  data: Uint8Array,
-  options: {
-    algorithm: string;
-    hash: string;
-    salt?: string;
-  }
+  data: BufferSource,
+  options: any
 ) => {
   // grab the user's keyfile
   const keyfile = await getActiveKeyfile().catch(() => {
@@ -21,54 +17,82 @@ const background: ModuleFunction<string> = async (
     throw new Error("No wallets added");
   });
 
-  // get decryption key
-  const decryptJwk = {
-    ...keyfile,
-    alg: "RSA-OAEP-256",
-    ext: true
-  };
-  const key = await crypto.subtle.importKey(
-    "jwk",
-    decryptJwk,
-    {
-      name: options.algorithm,
-      hash: {
-        name: options.hash
-      }
-    },
-    false,
-    ["decrypt"]
-  );
+  if (options.algorithm) {
+    // old ArConnect algorithm
+    // get decryption key
+    const decryptJwk = {
+      ...keyfile,
+      alg: "RSA-OAEP-256",
+      ext: true
+    };
+    const key = await crypto.subtle.importKey(
+      "jwk",
+      decryptJwk,
+      {
+        name: options.algorithm,
+        hash: {
+          name: options.hash
+        }
+      },
+      false,
+      ["decrypt"]
+    );
 
-  // prepare encrypted data
-  const encryptedKey = new Uint8Array(
-    new Uint8Array(Object.values(data)).slice(0, 512)
-  );
-  const encryptedData = new Uint8Array(
-    new Uint8Array(Object.values(data)).slice(512)
-  );
+    // prepare encrypted data
+    const encryptedKey = new Uint8Array(
+      new Uint8Array(Object.values(data)).slice(0, 512)
+    );
+    const encryptedData = new Uint8Array(
+      new Uint8Array(Object.values(data)).slice(512)
+    );
 
-  // create arweave client
-  const arweave = new Arweave(defaultGateway);
+    // create arweave client
+    const arweave = new Arweave(defaultGateway);
 
-  // decrypt data
-  const symmetricKey = await crypto.subtle.decrypt(
-    { name: options.algorithm },
-    key,
-    encryptedKey
-  );
+    // decrypt data
+    const symmetricKey = await crypto.subtle.decrypt(
+      {
+        name: options.algorithm
+      },
+      key,
+      encryptedKey
+    );
 
-  const res = await arweave.crypto.decrypt(
-    encryptedData,
-    new Uint8Array(symmetricKey)
-  );
+    const res = await arweave.crypto.decrypt(
+      encryptedData,
+      new Uint8Array(symmetricKey)
+    );
 
-  // if a salt is present, split it from the decrypted string
-  if (options.salt) {
-    return arweave.utils.bufferToString(res).split(options.salt)[0];
+    // if a salt is present, split it from the decrypted string
+    if (options.salt) {
+      return arweave.utils.bufferToString(res).split(options.salt)[0];
+    }
+
+    return arweave.utils.bufferToString(res);
+  } else if (options.name) {
+    // standard RSA decryption, from arweave.app
+    const decryptJwk = {
+      ...keyfile,
+      alg: undefined,
+      key_ops: undefined,
+      ext: true
+    };
+    const key = await crypto.subtle.importKey(
+      "jwk",
+      decryptJwk,
+      {
+        name: "RSA-OAEP",
+        hash: "SHA-256"
+      },
+      false,
+      ["decrypt"]
+    );
+    const decrypted = await window.crypto.subtle.decrypt(options, key, data);
+
+    return new Uint8Array(decrypted);
+  } else {
+    throw new Error("Invalid options passed", options);
   }
-
-  return arweave.utils.bufferToString(res);
 };
 
 export default background;
--- src/api/modules/decrypt/decrypt.foreground.ts
+++ src/api/modules/decrypt/decrypt.foreground.ts
@@ -1,6 +1,11 @@
 import type { ModuleFunction } from "~api/module";
 
-// no need to transform anything in the foreground
-const foreground: ModuleFunction<void> = () => {};
+const foreground: ModuleFunction<void> = (_, options) => {
+  if (options.algorithm) {
+    console.warn(
+      '[ArConnect] YOU\'RE USING DEPRECATED PARAMS FOR "decrypt()". Please check the documentation.\nhttps://github.com/arconnectio/ArConnect#decryptdata-options-promisestring'
+    );
+  }
+};
 
 export default foreground;
