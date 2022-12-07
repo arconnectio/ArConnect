648d4e6a4f97614d0a28d8b00988745ef0d72909
Marton Lederer <marton@lederer.hu>
2022-12-07 11:44:04 +0100

    feat: balance display in token route

--- src/routes/popup/token/[id].tsx
+++ src/routes/popup/token/[id].tsx
@@ -1,9 +1,17 @@
-import { EyeIcon, MessageIcon, ShareIcon } from "@iconicicons/react";
+import {
+  ArrowDownLeftIcon,
+  ArrowUpRightIcon,
+  EyeIcon,
+  MessageIcon,
+  ShareIcon
+} from "@iconicicons/react";
 import { AnimatePresence, motion, Variants } from "framer-motion";
 import { Section, Spacer, Text } from "@arconnect/components";
+import { useStorage } from "@plasmohq/storage/hook";
 import { useMemo, useRef, useState } from "react";
 import { getCommunityUrl } from "~utils/format";
 import { getTokenLogo } from "~lib/viewblock";
+import { useLocation } from "wouter";
 import PeriodPicker from "~components/popup/asset/PeriodPicker";
 import PriceChart from "~components/popup/asset/PriceChart";
 import TokenLoading from "~components/popup/asset/Loading";
@@ -21,12 +29,14 @@ export default function Asset({ id }: Props) {
   // price period
   const [period, setPeriod] = useState("Day");
 
+  // community settings
   const settings = useMemo(() => {
     if (!state || !state.settings) return undefined;
 
     return new Map(state.settings);
   }, [state]);
 
+  // chat link urls
   const chatLinks = useMemo<string[]>(() => {
     const val = settings?.get("communityDiscussionLinks");
 
@@ -34,6 +44,25 @@ export default function Asset({ id }: Props) {
 
     return val as string[];
   }, [settings]);
+
+  // current address
+  const [activeAddress, setActiveAddress] = useStorage<string>({
+    key: "active_address",
+    area: "local",
+    isSecret: true
+  });
+
+  // balance in units of the token
+  const tokenBalance = useMemo(() => {
+    if (!state) return "0";
+
+    const val = state.balances?.[activeAddress] || 0;
+
+    return val.toLocaleString(undefined, { maximumFractionDigits: 2 });
+  }, [state, activeAddress]);
+
+  // location
+  const [, setLocation] = useLocation();
 
   return (
     <>
@@ -60,6 +89,31 @@ export default function Asset({ id }: Props) {
             </PriceChart>
             <Spacer y={0.15} />
             <Section>
+              <BalanceSection>
+                <div>
+                  <BalanceLabel>
+                    {browser.i18n.getMessage("your_balance")}
+                  </BalanceLabel>
+                  <Spacer y={0.38} />
+                  <TokenBalance>
+                    {tokenBalance}
+                    <span>{state.ticker || ""}</span>
+                  </TokenBalance>
+                  {/*<FiatBalance>
+                    $13.45 USD
+                  </FiatBalance>*/}
+                  <FiatBalance>$?? USD</FiatBalance>
+                </div>
+                <TokenActions>
+                  <TokenAction onClick={() => setLocation(`/send/${id}`)} />
+                  <ActionSeparator />
+                  <TokenAction
+                    as={ArrowDownLeftIcon}
+                    onClick={() => setLocation("/receive")}
+                  />
+                </TokenActions>
+              </BalanceSection>
+              <Spacer y={1.45} />
               <Title noMargin>{browser.i18n.getMessage("about_title")}</Title>
               <Spacer y={0.6} />
               <Description>
@@ -67,7 +121,7 @@ export default function Asset({ id }: Props) {
                   state.description ||
                   browser.i18n.getMessage("no_description")}
               </Description>
-              <Spacer y={1} />
+              <Spacer y={1.45} />
               <Title noMargin>{browser.i18n.getMessage("info_title")}</Title>
               <Spacer y={0.6} />
               {chatLinks.map((link, i) => (
@@ -114,6 +168,75 @@ const chartAnimation: Variants = {
   hidden: { opacity: 0 },
   shown: { opacity: 1 }
 };
+
+const BalanceSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const BalanceLabel = styled(Text).attrs({
+  noMargin: true
+})`
+  font-size: 0.69rem;
+`;
+
+const TokenBalance = styled(Text).attrs({
+  noMargin: true,
+  heading: true
+})`
+  display: flex;
+  gap: 0.3rem;
+  align-items: baseline;
+  font-size: 1.85rem;
+  line-height: 1.1em;
+
+  span {
+    font-size: 0.57em;
+    text-transform: uppercase;
+  }
+`;
+
+const FiatBalance = styled(Text).attrs({
+  noMargin: true
+})`
+  color: rgb(${(props) => props.theme.primaryText});
+  font-weight: 600;
+  font-size: 0.74rem;
+`;
+
+const TokenActions = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: rgba(${(props) => props.theme.theme}, 0.15);
+  padding: 0.55rem 0.74rem;
+  gap: 0.74rem;
+  border-radius: 14px;
+`;
+
+const TokenAction = styled(ArrowUpRightIcon)`
+  font-size: 1.45rem;
+  width: 1em;
+  height: 1em;
+  cursor: pointer;
+  color: rgb(${(props) => props.theme.theme});
+  transition: all 0.23s ease-in-out;
+
+  &:hover {
+    opacity: 0.85;
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+const ActionSeparator = styled.div`
+  width: 1.5px;
+  height: 1.25rem;
+  background-color: rgba(${(props) => props.theme.theme}, 0.3);
+  border-radius: 2px;
+`;
 
 const Description = styled(Text).attrs({
   noMargin: true
