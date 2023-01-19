6b1e45d0dece99e6994eb3fd057649c69ac28492
Marton Lederer <marton@lederer.hu>
2023-01-19 13:31:25 +0100

    fix: button height for input-inline-btns

--- src/components/arlocal/InputWrapper.tsx
+++ src/components/arlocal/InputWrapper.tsx
@@ -1,9 +1,21 @@
+import { IconButton } from "~components/IconButton";
+import { Button } from "@arconnect/components";
 import styled from "styled-components";
 
 export const InputWithBtn = styled.div`
   display: flex;
   align-items: flex-end;
   gap: 0.8rem;
+
+  ${Button} {
+    padding-top: 0.9rem;
+    padding-bottom: 0.9rem;
+    border-radius: 18px;
+  }
+
+  ${IconButton} {
+    padding: 0.9rem;
+  }
 `;
 
 export const InputWrapper = styled.div`
