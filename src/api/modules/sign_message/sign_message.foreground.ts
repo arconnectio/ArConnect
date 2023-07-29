83ba88e5d611c8c143be874c82d748a8ccd5c025
Marton Lederer <marton@lederer.hu>
2023-07-29 17:27:28 +0200

    feat: validate sign message foreground

--- src/api/modules/sign_message/sign_message.foreground.ts
+++ src/api/modules/sign_message/sign_message.foreground.ts
@@ -1,11 +1,17 @@
 import type { TransformFinalizer } from "~api/foreground";
 import type { SignMessageOptions } from "./types";
 import type { ModuleFunction } from "~api/module";
+import { isArrayBuffer } from "~utils/assertions";
 
 const foreground: ModuleFunction<any[]> = (
   data: ArrayBuffer,
   options?: SignMessageOptions
-) => [Object.values(data), options];
+) => {
+  // validate
+  isArrayBuffer(data);
+
+  return [Object.values(data), options];
+};
 
 export const finalizer: TransformFinalizer<number[], any, any> = (result) =>
   new Uint8Array(result);
