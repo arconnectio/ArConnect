aad420778043541df9870db21667f6d1a63069cd
Marton Lederer <marton@lederer.hu>
2023-08-15 13:13:03 +0200

    fix: migration + password not existing

--- src/components/welcome/load/Migrate.tsx
+++ src/components/welcome/load/Migrate.tsx
@@ -1,8 +1,10 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@iconicicons/react";
 import type { JWKInterface } from "arweave/node/lib/wallet";
 import { Spacer, Text } from "@arconnect/components";
 import { useWalletsDetails } from "~wallets/hooks";
 import browser from "webextension-polyfill";
 import styled from "styled-components";
+import { useState } from "react";
 import Wallet from "./Wallet";
 
 export default function Migrate({
@@ -11,6 +13,7 @@ export default function Migrate({
   onMigrateClick
 }: Props) {
   const walletDetails = useWalletsDetails(wallets);
+  const [showAll, setShowAll] = useState(false);
 
   return (
     <>
@@ -22,16 +25,35 @@ export default function Migrate({
       </Title>
       <Spacer y={0.6} />
       <Wallets>
-        {walletDetails.map((wallet, i) => (
-          <Wallet address={wallet.address} label={wallet.label} key={i} />
-        ))}
+        {walletDetails
+          .slice(0, showAll ? walletDetails.length : 5)
+          .map((wallet, i) => (
+            <Wallet address={wallet.address} label={wallet.label} key={i} />
+          ))}
       </Wallets>
       {noMigration && (
         <>
           <Spacer y={0.475} />
-          <MigrateAnyway noMargin onClick={onMigrateClick}>
-            {browser.i18n.getMessage("migrate_anyway")}
-          </MigrateAnyway>
+          <MigrateFlex>
+            <LinkBtn noMargin onClick={onMigrateClick}>
+              {browser.i18n.getMessage("migrate_anyway")}
+            </LinkBtn>
+            {walletDetails.length > 5 && (
+              <LinkBtn noMargin onClick={() => setShowAll((val) => !val)}>
+                {browser.i18n.getMessage("view_all")}
+                {(showAll && <ChevronUpIcon />) || <ChevronDownIcon />}
+              </LinkBtn>
+            )}
+          </MigrateFlex>
+        </>
+      )}
+      {walletDetails.length > 5 && !noMigration && (
+        <>
+          <Spacer y={0.475} />
+          <LinkBtn noMargin onClick={() => setShowAll((val) => !val)}>
+            {browser.i18n.getMessage("view_all")}
+            {(showAll && <ChevronUpIcon />) || <ChevronDownIcon />}
+          </LinkBtn>
         </>
       )}
     </>
@@ -44,13 +66,28 @@ const Title = styled(Text).attrs({
   color: rgb(${(props) => props.theme.theme});
 `;
 
-const MigrateAnyway = styled(Title)`
+const MigrateFlex = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LinkBtn = styled(Title)`
+  display: flex;
+  align-items: center;
   cursor: pointer;
   width: max-content;
+  gap: 0.1rem;
   transition: all 0.23s ease-in-out;
 
   &:hover {
     opacity: 0.8;
+  }
+
+  svg {
+    font-size: 1.2em;
+    width: 1em;
+    height: 1em;
   }
 `;
 
