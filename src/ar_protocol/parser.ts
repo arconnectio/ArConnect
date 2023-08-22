034b4012daab3e47c29792cb24d0c8f8fbf75bc9
Marton Lederer <marton@lederer.hu>
2023-08-22 18:09:30 +0200

    fix: ar:// protocol interception

--- src/ar_protocol/parser.ts
+++ src/ar_protocol/parser.ts
@@ -1,6 +1,10 @@
 import { concatGatewayURL, type Gateway } from "~applications/gateway";
 import { isAddressFormat } from "~utils/format";
 
+/**
+ * Get gateway redirect URL for ar:// protocol
+ * or return false if it is not a protocol request.
+ */
 export function getRedirectURL(url: URL, gateway: Gateway) {
   let redirectURL: string;
 
@@ -8,21 +12,21 @@ export function getRedirectURL(url: URL, gateway: Gateway) {
   const arURL = url.searchParams.get("q");
 
   if (!arURL || arURL === "") {
-    return redirectURL;
+    return false;
   }
 
   // value (address / permapage / id)
   const value = arURL.replace("ar://", "");
 
   if (!value || value === "") {
-    return redirectURL;
+    return false;
   }
 
   redirectURL = concatGatewayURL(gateway) + "/" + value;
 
   // if it is not an Arweave ID, redirect to permapages
   if (!isAddressFormat(value)) {
-    redirectURL = `https://${value}.arweave.dev`;
+    redirectURL = `${gateway.protocol}://${value}.${gateway.host}:${gateway.port}`;
   }
 
   return redirectURL;
