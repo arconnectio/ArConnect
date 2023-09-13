1b35c158c987a08a1f0a1649c9306e1da7043d68
Nicholas Ma <nicholas.maa@gmail.com>
2023-09-13 16:11:16 -0700

    feat: wallet header shows user address if default named

--- src/components/popup/WalletHeader.tsx
+++ src/components/popup/WalletHeader.tsx
@@ -8,7 +8,7 @@ import HardwareWalletIcon, {
 import { useHardwareApi } from "~wallets/hooks";
 import { type Gateway } from "~applications/gateway";
 import type { StoredWallet } from "~wallets";
-import { getAppURL } from "~utils/format";
+import { formatAddress, getAppURL } from "~utils/format";
 import { removeApp } from "~applications";
 import { useAnsProfile } from "~lib/ans";
 import { useTheme } from "~utils/theme";
@@ -83,8 +83,14 @@ export default function WalletHeader() {
   const walletName = useMemo(() => {
     if (!ansProfile?.label) {
       const wallet = wallets.find(({ address }) => address === activeAddress);
+      let name = wallet?.nickname || "Wallet";
 
-      return wallet?.nickname || "Wallet";
+      const address = wallet?.address && formatAddress(wallet?.address, 4);
+      if (/^Account \d+$/.test(name)) {
+        name = address;
+      }
+
+      return name;
     }
 
     return ansProfile.label;
