31c557d31aff9e19a1611021e639bf17cca402f3
Marton Lederer <marton@lederer.hu>
2023-09-17 16:50:32 +0200

    feat: finish up send screen

--- src/routes/popup/send/index.tsx
+++ src/routes/popup/send/index.tsx
@@ -4,12 +4,14 @@ import styled, { css } from "styled-components";
 import { Button, Section, Spacer, Text } from "@arconnect/components";
 import browser from "webextension-polyfill";
 import Head from "~components/popup/Head";
+import * as viewblock from "~lib/viewblock";
 import {
   ArrowUpRightIcon,
   ChevronRightIcon,
   RefreshIcon
 } from "@iconicicons/react";
-import {
+import Token, {
+  ArToken,
   Logo,
   LogoAndDetails,
   LogoWrapper,
@@ -17,6 +19,7 @@ import {
 } from "~components/popup/Token";
 import useSetting from "~settings/hook";
 import {
+  balanceToFractioned,
   formatFiatBalance,
   formatTokenBalance,
   getCurrencySymbol
@@ -24,16 +27,41 @@ import {
 import { useStorage } from "@plasmohq/storage/hook";
 import { ExtensionStorage } from "~utils/storage";
 import { useTokens } from "~tokens";
-import { Token } from "~tokens/token";
+import { loadTokenLogo, type Token as TokenInterface } from "~tokens/token";
+import { useTheme } from "~utils/theme";
+import arLogoLight from "url:/assets/ar/logo_light.png";
+import arLogoDark from "url:/assets/ar/logo_dark.png";
+import Arweave from "arweave";
+import { defaultGateway } from "~applications/gateway";
+import { useBalance } from "~wallets/hooks";
+import { getPrice } from "~lib/coingecko";
+import { getContract } from "~lib/warp";
+import redstone from "redstone-api";
+import { AnimatePresence, motion, type Variants } from "framer-motion";
+import Collectible from "~components/popup/Collectible";
 
 // default size for the qty text
 const defaulQtytSize = 3.7;
+const arPlaceholder: TokenInterface = {
+  id: "AR",
+  name: "Arweave",
+  ticker: "AR",
+  type: "asset",
+  balance: 0,
+  decimals: 12
+};
 
-export default function Send({ id = "AR" }: Props) {
+export default function Send({ id }: Props) {
   // Segment
   useEffect(() => {
     trackPage(PageType.SEND);
   }, []);
+
+  // active address
+  const [activeAddress] = useStorage<string>({
+    key: "active_address",
+    instance: ExtensionStorage
+  });
 
   // quantity
   const [qty, setQty] = useStorage<string>(
@@ -43,14 +71,6 @@ export default function Send({ id = "AR" }: Props) {
     },
     ""
   );
-
-  // qty text size
-  const qtySize = useMemo(() => {
-    const maxLengthDef = 5;
-
-    if (qty.length <= maxLengthDef) return defaulQtytSize;
-    return defaulQtytSize / (qty.length / maxLengthDef);
-  }, [qty]);
 
   // currency setting
   const [currency] = useSetting<string>("currency");
@@ -68,39 +88,178 @@ export default function Send({ id = "AR" }: Props) {
   const tokens = useTokens();
 
   // token that the user is going to send
-  const [tokenID, setTokenID] = useState<"AR" | string>(id);
-  const token = useMemo<Token>(
-    () =>
-      tokens.find((t) => {
-        if (tokenID === "AR")
-          return {
-            id: "AR",
-            name: "Arweave",
-            ticker: "AR",
-            type: "asset",
-            balance: 0,
-            decimals: 12
-          };
+  const [tokenID, setTokenID] = useStorage<"AR" | string>(
+    {
+      key: "last_send_token",
+      instance: ExtensionStorage
+    },
+    "AR"
+  );
 
-        return t.id === tokenID;
-      }),
+  const token = useMemo<TokenInterface>(
+    () => tokens.find((t) => t.id === tokenID) || arPlaceholder,
     [tokenID]
   );
 
+  // if the ID is defined on mount, that means that
+  // we need to reset the qty field
+  useEffect(() => {
+    if (!id) return;
+    setTokenID(id);
+    setQty("");
+  }, []);
+
+  // token logo
+  const [logo, setLogo] = useState<string>();
+  const theme = useTheme();
+
+  useEffect(() => {
+    (async () => {
+      if (!token.id || logo) return;
+
+      setLogo(viewblock.getTokenLogo(token.id));
+      setLogo(await loadTokenLogo(token.id, token.defaultLogo, theme));
+    })();
+  }, [theme, logo, token]);
+
+  //arweave logo
+  const arweaveLogo = useMemo(
+    () => (theme === "light" ? arLogoLight : arLogoDark),
+    [theme]
+  );
+
+  // balance
   const [balance, setBalance] = useState(0);
+  const arBalance = useBalance();
+
+  useEffect(() => {
+    (async () => {
+      if (token.id === "AR") {
+        return setBalance(arBalance);
+      }
+
+      const res = await getContract<number>(token.id, {
+        query: `$.balances.${activeAddress}`
+      });
+
+      setBalance(
+        balanceToFractioned(res.result[0], {
+          id: token.id,
+          decimals: token.decimals,
+          divisibility: token.divisibility
+        })
+      );
+    })();
+  }, [token, activeAddress, arBalance]);
+
+  // token price
   const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      if (token.id === "AR") {
+        const arPrice = await getPrice("arweave", currency);
+
+        return setPrice(arPrice);
+      }
+
+      // get price from redstone
+      const res = await redstone.getPrice(token.ticker);
+
+      if (!res.value) {
+        return setPrice(0);
+      }
+
+      // get price in currency
+      const multiplier =
+        currency !== "usd" ? await getPrice("usd", currency) : 1;
+
+      setPrice(res.value * multiplier);
+    })();
+  }, [token, currency]);
+
+  // quantity in the other currency
+  const secondaryQty = useMemo(() => {
+    const qtyParsed = parseFloat(qty) || 0;
+
+    if (qtyMode === "token") return qtyParsed * price;
+    else return qtyParsed * (1 / price);
+  }, [qty, qtyMode, price]);
+
+  // network fee
+  const [networkFee, setNetworkFee] = useState<string>("0");
+
+  useEffect(() => {
+    (async () => {
+      if (token.id !== "AR") {
+        return setNetworkFee("0");
+      }
+
+      const arweave = new Arweave(defaultGateway);
+      const txPrice = await arweave.transactions.getPrice(0, "dummyTarget");
+
+      setNetworkFee(arweave.ar.winstonToAr(txPrice));
+    })();
+  }, [token]);
+
+  // maximum possible send amount
+  const max = useMemo(() => {
+    let maxAmountToken = balance - parseFloat(networkFee);
+
+    if (token.id !== "AR") maxAmountToken = balance;
+
+    return maxAmountToken * (qtyMode === "fiat" ? price : 1);
+  }, [balance, token, networkFee, qtyMode]);
+
+  // switch back to token qty mode if the
+  // token does not have a fiat price
+  useEffect(() => {
+    if (!!price) return;
+    setQtyMode("token");
+  }, [price]);
+
+  // switch between fiat qty mode / token qty mode
+  function switchQtyMode() {
+    if (!price) return;
+    setQty(secondaryQty.toString());
+    setQtyMode((val) => (val === "fiat" ? "token" : "fiat"));
+  }
+
+  // invalid qty
+  const invalidQty = useMemo(() => {
+    const parsedQty = parseFloat(qty) || 0;
+
+    return parsedQty < 0 || parsedQty > max;
+  }, [qty, max]);
+
+  // show token selector
+  const [showTokenSelector, setShownTokenSelector] = useState(false);
+
+  function updateSelectedToken(id: string) {
+    setTokenID(id);
+    setLogo(undefined);
+    setShownTokenSelector(false);
+  }
+
+  // qty text size
+  const qtySize = useMemo(() => {
+    const maxLengthDef = 7;
+    const symbol =
+      qtyMode === "token" ? token.ticker : getCurrencySymbol(currency);
+    const qtyLength = qty === "" ? 4 : qty.length;
+    const qtyLengthWithSymbol = qtyLength + symbol.length;
+
+    if (qtyLengthWithSymbol <= maxLengthDef) return defaulQtytSize;
+    return defaulQtytSize / (qtyLengthWithSymbol / maxLengthDef);
+  }, [qty, qtyMode, currency, token]);
 
   return (
     <Wrapper>
       <div>
         <Head title={browser.i18n.getMessage("send")} />
         <Spacer y={1} />
-        <QuantitySection qtyMode={qtyMode}>
-          <Switch
-            onClick={() =>
-              setQtyMode((val) => (val === "fiat" ? "token" : "fiat"))
-            }
-          />
+        <QuantitySection qtyMode={qtyMode} invalidValue={invalidQty}>
+          <Switch disabled={!price} onClick={switchQtyMode} />
           {qtyMode === "fiat" && (
             <Ticker style={{ fontSize: `${qtySize}rem` }}>
               {getCurrencySymbol(currency)}
@@ -140,40 +299,86 @@ export default function Send({ id = "AR" }: Props) {
             </Imitate>
           </Quantity>
           {qtyMode === "token" && (
-            <Ticker style={{ fontSize: `${qtySize}rem` }}>AR</Ticker>
+            <Ticker style={{ fontSize: `${qtySize}rem` }}>
+              {token.ticker.toUpperCase()}
+            </Ticker>
           )}
-          <Max>Max</Max>
+          <Max onClick={() => setQty(max.toString())}>Max</Max>
         </QuantitySection>
         <Spacer y={1} />
         <Datas>
+          {!!price && (
+            <Text noMargin>
+              ≈
+              {qtyMode === "fiat"
+                ? formatTokenBalance(secondaryQty)
+                : formatFiatBalance(secondaryQty, currency)}
+              {qtyMode === "fiat" && " " + token.ticker}
+            </Text>
+          )}
           <Text noMargin>
-            ≈
-            {qtyMode === "token"
-              ? formatTokenBalance(2345.43)
-              : formatFiatBalance(2345.43, currency)}
-            {qtyMode === "token" && " AR"}
+            ~{networkFee}
+            {" AR "}
+            {browser.i18n.getMessage("network_fee")}
           </Text>
-          <Text noMargin>~0.0000043 AR network fee</Text>
         </Datas>
       </div>
       <BottomActions>
-        <TokenSelector>
+        <TokenSelector onClick={() => setShownTokenSelector(true)}>
           <LogoAndDetails>
             <LogoWrapper>
-              <Logo src="" />
+              <Logo src={logo || arweaveLogo} />
             </LogoWrapper>
-            <TokenName>Arweave</TokenName>
+            <TokenName>{token.name || token.ticker}</TokenName>
           </LogoAndDetails>
           <TokenSelectorRightSide>
             <Text noMargin>{browser.i18n.getMessage("setting_currency")}</Text>
             <ChevronRightIcon />
           </TokenSelectorRightSide>
         </TokenSelector>
-        <Button>
-          Send
+        <Button disabled={invalidQty || parseFloat(qty) === 0 || qty === ""}>
+          {browser.i18n.getMessage("send")}
           <ArrowUpRightIcon />
         </Button>
       </BottomActions>
+      <AnimatePresence>
+        {showTokenSelector && (
+          <TokenSelectorWrapper
+            variants={animation}
+            initial="hidden"
+            animate="shown"
+            exit="hidden"
+          >
+            <TokensSection>
+              <ArToken onClick={() => updateSelectedToken("AR")} />
+              {tokens
+                .filter((token) => token.type === "asset")
+                .map((token, i) => (
+                  <Token
+                    {...token}
+                    onClick={() => updateSelectedToken(token.id)}
+                    key={i}
+                  />
+                ))}
+            </TokensSection>
+            <CollectiblesList>
+              {tokens
+                .filter((token) => token.type === "collectible")
+                .map((token, i) => (
+                  <Collectible
+                    id={token.id}
+                    name={token.name || token.ticker}
+                    balance={token.balance}
+                    divisibility={token.divisibility}
+                    decimals={token.decimals}
+                    onClick={() => updateSelectedToken(token.id)}
+                    key={i}
+                  />
+                ))}
+            </CollectiblesList>
+          </TokenSelectorWrapper>
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 }
@@ -192,13 +397,21 @@ interface Props {
 
 type QtyMode = "fiat" | "token";
 
-const QuantitySection = styled.div<{ qtyMode: QtyMode }>`
+const QuantitySection = styled.div<{ qtyMode: QtyMode; invalidValue: boolean }>`
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: ${(props) => (props.qtyMode === "token" ? "0.65rem" : "0")};
   height: ${defaulQtytSize + "rem"};
+
+  input,
+  p {
+    color: rgb(
+      ${(props) => (props.invalidValue ? "255, 0, 0" : props.theme.theme)}
+    );
+    transition: color 0.23s ease-in-out;
+  }
 `;
 
 const Quantity = styled.div`
@@ -224,7 +437,6 @@ const QuantityInput = styled.input.attrs({
   background-color: transparent;
   padding: 0;
   z-index: 10;
-  color: rgb(${(props) => props.theme.theme});
   text-align: ${(props) => (props.qtyMode === "token" ? "right" : "left")};
   ${qtyTextStyle}
 `;
@@ -238,7 +450,6 @@ const Imitate = styled.p`
 `;
 
 const Ticker = styled.p`
-  color: rgb(${(props) => props.theme.theme});
   margin: 0;
   text-transform: uppercase;
   ${qtyTextStyle}
@@ -289,11 +500,13 @@ const Max = styled(Text).attrs({
   ${floatingAction}
 `;
 
-const Switch = styled(RefreshIcon)`
+const Switch = styled(RefreshIcon)<{ disabled: boolean }>`
   font-size: 1.45rem;
   width: 1em;
   height: 1em;
   left: 20px;
+  opacity: ${(props) => (props.disabled ? ".7" : "1")};
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")} !important;
   ${floatingAction}
 `;
 
@@ -332,4 +545,46 @@ const TokenSelectorRightSide = styled.div`
     text-transform: uppercase;
     font-size: 0.7rem;
   }
+`;
+
+const TokenSelectorWrapper = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow-y: auto;
+  background-color: rgb(${(props) => props.theme.background});
+  z-index: 1000;
+`;
+
+const animation: Variants = {
+  hidden: { opacity: 0 },
+  shown: { opacity: 1 }
+};
+
+const expandAnimation: Variants = {
+  hidden: {
+    opacity: 0,
+    height: 0
+  },
+  shown: {
+    opacity: 1,
+    height: "auto"
+  }
+};
+
+const TokensSection = styled(Section)`
+  display: flex;
+  flex-direction: column;
+  gap: 0.82rem;
+  padding-top: 2rem;
+  padding-bottom: 1.4rem;
+`;
+
+const CollectiblesList = styled(Section)`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.2rem;
+  padding-top: 0;
 `;
