01e7727ecafadbb09d865546eb16679d87d1863f
nicholas ma <nicholas.maa@gmail.com>
2023-09-19 21:25:24 -0700

    fix: adds opt in to generate page, remove logs

--- src/routes/welcome/generate/done.tsx
+++ src/routes/welcome/generate/done.tsx
@@ -1,4 +1,4 @@
-import { Button, Spacer, Text } from "@arconnect/components";
+import { Button, Checkbox, Spacer, Text } from "@arconnect/components";
 import { PasswordContext, WalletContext } from "../setup";
 import { type AnsUser, getAnsProfile } from "~lib/ans";
 import { formatAddress } from "~utils/format";
@@ -7,6 +7,9 @@ import browser from "webextension-polyfill";
 import { addWallet } from "~wallets";
 import { useContext, useEffect } from "react";
 import { EventType, PageType, trackEvent, trackPage } from "~utils/analytics";
+import useSetting from "~settings/hook";
+import { ExtensionStorage } from "~utils/storage";
+import { useStorage } from "@plasmohq/storage/hook";
 
 export default function Done() {
   // wallet context
@@ -14,6 +17,11 @@ export default function Done() {
 
   // password
   const { password } = useContext(PasswordContext);
+  const [analytics, setAnalytics] = useSetting<boolean>("analytics");
+  const [answered, setAnswered] = useStorage<boolean>({
+    key: "analytics_consent_answered",
+    instance: ExtensionStorage
+  });
 
   // add generated wallet
   async function done() {
@@ -39,6 +47,11 @@ export default function Done() {
     // log user onboarded
     await trackEvent(EventType.ONBOARDED, {});
 
+    if (!analytics && !answered) {
+      await setAnswered(true);
+      await setAnalytics(false);
+    }
+
     // close window
     window.top.close();
   }
@@ -56,6 +69,15 @@ export default function Done() {
           formatAddress(wallet.address || "", 6)
         ])}
       </Paragraph>
+      <Checkbox
+        checked={!!analytics}
+        onChange={(checked) => {
+          setAnalytics(checked);
+          setAnswered(true);
+        }}
+      >
+        {browser.i18n.getMessage("analytics_title")}
+      </Checkbox>
       <Spacer y={3} />
       <Button fullWidth onClick={done}>
         {browser.i18n.getMessage("done")}
