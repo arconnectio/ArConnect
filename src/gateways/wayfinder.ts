43767d2d080547e0808da2477686992ad550e7a2
Marton Lederer <marton@lederer.hu>
2023-09-27 21:00:27 +0200

    feat: wayfinder skeleton

---
+++ src/gateways/wayfinder.ts
@@ -0,0 +1,51 @@
+import { defaultGateway, Gateway } from "./gateway";
+import { useEffect, useState } from "react";
+
+/**
+ * Selects the appropriate gateway for the provided usecase.
+ */
+export async function findGateway(
+  requirements: Requirements
+): Promise<Gateway> {}
+
+/**
+ * Gateway hook that uses wayfinder to select the active gateway.
+ */
+export function useGateway(requirements: Requirements) {
+  // currently active gw
+  const [activeGateway, setActiveGateway] = useState<Gateway>(defaultGateway);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // select recommended gateway using wayfinder
+        const recommended = await findGateway(requirements);
+
+        setActiveGateway(recommended);
+      } catch {}
+    })();
+  }, [requirements]);
+
+  return activeGateway;
+}
+
+interface Requirements {
+  /* Whether the gateway should support GraphQL requests */
+  graphql?: boolean;
+  /* Should the gateway support ArNS */
+  arns?: boolean;
+  /**
+   * The block where the gateway should start syncing data from.
+   * Set for 0 to include all blocks.
+   * If undefined, wayfinder will not ensure that the start block
+   * is 0.
+   */
+  startBlock?: number;
+  /**
+   * Ensure that the gateway has a high stake. This is required
+   * with data that is important to be accurate. If true, wayfinder
+   * will make sure that the gateway stake is higher than the
+   * average stake of ar.io nodes.
+   */
+  ensureStake?: boolean;
+}
