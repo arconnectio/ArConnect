e4d45a72808d6fa95c5e706d4a7aac5d722c8eaf
ETH1Elohim <ezmax.defi@gmail.com>
2023-11-18 15:30:45 -0700

    styling: fiat select dropdown

--- src/routes/popup/purchase.tsx
+++ src/routes/popup/purchase.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { useHistory } from "~utils/hash_router";
 import browser from "webextension-polyfill";
 import styled from "styled-components";
 import { hoverEffect } from "~utils/theme";
-import { CloseIcon } from "@iconicicons/react";
-import { Section } from "@arconnect/components";
+import { CloseIcon, ChevronDownIcon } from "@iconicicons/react";
+import { Section, Input, Card } from "@arconnect/components";
 import BuyButton from "~components/popup/home/BuyButton";
 
 export default function Purchase() {
   const [push] = useHistory();
+
+  const [fiatSwitchOpen, setFiatSwitchOpen] = useState(false);
 
   return (
     <Wrapper>
@@ -20,13 +23,65 @@ export default function Purchase() {
             </ExitIcon>
           </BackWrapper>
         </Header>
+        <MainSwap>
+          <FiatWrapper>
+            <FiatSelect
+              onClick={() => setFiatSwitchOpen(true)}
+              open={fiatSwitchOpen}
+            >
+              $USD
+              <SelectIcon />
+            </FiatSelect>
+          </FiatWrapper>
+        </MainSwap>
       </div>
       <Section>
-        <BuyButton padding={true} route={"/confirm-purchase"} logo={false} />
+        <BuyButton padding={false} route={"/confirm-purchase"} logo={false} />
       </Section>
     </Wrapper>
   );
 }
+
+const FiatWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SelectIcon = styled(ChevronDownIcon)`
+  width: 37px;
+  height: 37px;
+  color: white;
+`;
+
+const FiatSelect = styled(Card)<{ open: boolean }>`
+  position: relative;
+  display: flex;
+  width: 84px;
+  height: 38px;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  background-color: #ab9aff;
+  transition: all 0.23s ease-in-out;
+  font-size: 16px;
+  border-radius: 12px;
+  padding: 2px 3px 2px 10px;
+  cursor: pointer;
+  font-weight: 500;
+
+  ${(props) =>
+    props.open
+      ? `border-color: rgba(${props.theme.theme}, .5); box-shadow: 0 0 0 1px rgba(${props.theme.theme}, .5);`
+      : ""}
+
+  ${SelectIcon} {
+    transform: ${(props) => (props.open ? "rotate(180deg)" : "rotate(0)")};
+  }
+`;
+
+const ReceiveInput = styled.div``;
 
 const Wrapper = styled.div`
   display: flex;
@@ -74,9 +129,8 @@ const ExitIcon = styled(CloseIcon)`
   width: 30px;
 `;
 
-// const MainSwap = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   height: 303px;
-//   padding: 0px 12px 4.8px 12px;
-// `;
+const MainSwap = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 12px 4.8px 12px;
+`;
