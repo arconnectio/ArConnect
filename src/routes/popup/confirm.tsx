0e537cc276be098e52095665a69ffd58ff3d61bc
ETH1Elohim <ezmax.defi@gmail.com>
2023-11-28 17:05:16 -0700

    add core components

--- src/routes/popup/confirm.tsx
+++ src/routes/popup/confirm.tsx
@@ -10,9 +10,28 @@ import type { DisplayTheme } from "@arconnect/components";
 import BuyButton from "~components/popup/home/BuyButton";
 
 export default function ConfirmPurchase() {
+  const [push] = useHistory();
+
   const theme = useTheme();
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <div>
+        <Header>
+          <Title>{browser.i18n.getMessage("confirm_purchase_title")}</Title>
+          <BackWrapper>
+            <ExitIcon onClick={() => push("/purchase")}>
+              {browser.i18n.getMessage("exit_buy_screen")}
+            </ExitIcon>
+          </BackWrapper>
+        </Header>
+        <MainContent></MainContent>
+      </div>
+      <Section>
+        <BuyButton padding={false} route={"/confirm-purchase"} logo={false} />
+      </Section>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
@@ -20,4 +39,51 @@ const Wrapper = styled.div`
   flex-direction: column;
   height: 100vh;
   justify-content: space-between;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 23.6px 12px 12.4px 12px;
+`;
+
+const Title = styled.div`
+  color: #ab9aff;
+  display: inline-block;
+  font-size: 22px;
+  font-weight: 500;
+`;
+
+const BackWrapper = styled.div`
+  position: relative;
+  display: flex;
+  width: max-content;
+  height: max-content;
+  cursor: pointer;
+
+  ${hoverEffect}
+
+  &::after {
+    width: 158%;
+    height: 158%;
+    border-radius: 100%;
+  }
+
+  &:active svg {
+    transform: scale(0.92);
+  }
+`;
+
+const ExitIcon = styled(CloseIcon)`
+  color: #ab9aff;
+  height: 30px;
+  width: 30px;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 12px 4.8px 12px;
 `;
