aefadea5041208e12b8bc39e24dfb955afe83a05
ETH1Elohim <ezmax.defi@gmail.com>
2023-11-30 11:56:30 -0700

    update: add new components

--- src/routes/popup/pending.tsx
+++ src/routes/popup/pending.tsx
@@ -4,7 +4,7 @@ import browser from "webextension-polyfill";
 import styled from "styled-components";
 import { ExtensionStorage } from "~utils/storage";
 import { useTheme, hoverEffect } from "~utils/theme";
-import { ArrowLeftIcon } from "@iconicicons/react";
+import { CheckIcon } from "@iconicicons/react";
 import { Section } from "@arconnect/components";
 import type { DisplayTheme } from "@arconnect/components";
 import BuyButton from "~components/popup/home/BuyButton";
@@ -14,27 +14,80 @@ export default function PendingPurchase() {
 
   const theme = useTheme();
 
+  const [orderID, setOrderID] = useState("");
+
   async function getActiveQuote() {
     const activeQuote = await ExtensionStorage.get("quote");
-
-    console.log(activeQuote);
-
-    console.log(activeQuote.quoteId);
 
     return activeQuote;
   }
 
+  useEffect(() => {
+    const quote = getActiveQuote();
+
+    setOrderID(quote.quoteId);
+  }, []);
+
   return (
     <Wrapper>
-      <div>
-        <MainContent></MainContent>
-      </div>
+      <MainContent>
+        <CheckGraphic>
+          <Check />
+        </CheckGraphic>
+        <CongratsTitle>
+          {browser.i18n.getMessage("congrats_purchase_pending")}
+        </CongratsTitle>
+        <PurchasePendingText>
+          {browser.i18n.getMessage("info_purchase_pending")}
+        </PurchasePendingText>
+        <OrderID>
+          {browser.i18n.getMessage("order_id_purchase_pending")} {orderID}
+        </OrderID>
+      </MainContent>
       <Section>
         <BuyButton closeBuyAR />
       </Section>
     </Wrapper>
   );
 }
+
+const Check = styled(CheckIcon)`
+  color: #ffffff;
+  width: 67px;
+  height: 53px;
+`;
+
+const CheckGraphic = styled.div`
+  border-radius: 100%;
+  height: 100px;
+  width: 100px;
+  background-color: #14d110;
+  border: 6.67px solid #14d1100d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
+const OrderID = styled.div`
+  font-size: 12px;
+  font-weight: 200;
+  color: #ffffff;
+`;
+
+const PurchasePendingText = styled.div`
+  font-weight: 500;
+  font-size: 14px;
+  color: #ffffff;
+  margin-bottom: 10px;
+`;
+
+const CongratsTitle = styled.div`
+  font-size: 24px;
+  font-weight: 500;
+  color: #ab9aff;
+  margin-bottom: 10px;
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -45,7 +98,10 @@ const Wrapper = styled.div`
 `;
 
 const MainContent = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
-  padding: 0px 12px 4.8px 12px;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 37px 4.8px 37px;
 `;
