3addbb37e8b6689b429a65eeddc38a1dbc834957
nicholas ma <nicholas.maa@gmail.com>
2023-12-15 15:47:35 -0800

    feat: added light mode for graph and nav buttons

--- src/components/popup/Navigation.tsx
+++ src/components/popup/Navigation.tsx
@@ -11,37 +11,34 @@ import { useHistory } from "~utils/hash_router";
 export const NavigationBar = () => {
   const [push] = useHistory();
   const [location] = useLocation();
-  const isVisible = location !== "/send/transfer" && location !== "/receive";
 
   return (
     <>
-      {location !== "/send/transfer" && location !== "/receive" && (
-        <NavigationWrapper>
-          <Buttons>
-            <Button active={location === "/"} onClick={() => push("/")}>
-              <ActionButton as={Home02} />
-            </Button>
-            <Button
-              active={location === "/send/transfer"}
-              onClick={() => push("/send/transfer")}
-            >
-              <ActionButton />
-            </Button>
-            <Button
-              active={location === "/receive"}
-              onClick={() => push("/receive")}
-            >
-              <ActionButton as={ArrowDownLeft} />
-            </Button>
-            <Button
-              active={location === "/explore"}
-              onClick={() => push("/explore")}
-            >
-              <ActionButton as={Compass03} />
-            </Button>
-          </Buttons>
-        </NavigationWrapper>
-      )}
+      <NavigationWrapper>
+        <Buttons>
+          <Button active={location === "/"} onClick={() => push("/")}>
+            <ActionButton as={Home02} />
+          </Button>
+          <Button
+            active={location === "/send/transfer"}
+            onClick={() => push("/send/transfer")}
+          >
+            <ActionButton />
+          </Button>
+          <Button
+            active={location === "/receive"}
+            onClick={() => push("/receive")}
+          >
+            <ActionButton as={ArrowDownLeft} />
+          </Button>
+          <Button
+            active={location === "/explore"}
+            onClick={() => push("/explore")}
+          >
+            <ActionButton as={Compass03} />
+          </Button>
+        </Buttons>
+      </NavigationWrapper>
     </>
   );
 };
@@ -70,7 +67,9 @@ const Button = styled.button<{ active: boolean }>`
 `;
 
 const ActionButton = styled(ArrowUpRight)`
-  color: #ebebf1;
+  color: ${(props) =>
+    props.theme.displayTheme === "light" ? "#AB9AFF" : "#ebebf1"};
+
   width: 1.9em;
   height: 1.9em;
   transition: all 0.23s ease-in-out;
@@ -89,15 +88,20 @@ const NavigationWrapper = styled.div`
   bottom: 0;
   width: 100%;
   height: 3.00625rem;
-  color: #ebebf1;
+  color: ${(props) =>
+    props.theme.displayTheme === "light" ? "#AB9AFF" : "#ebebf1"};
 
-  border-top: 1px solid #423d59;
-  border-left: 1px solid #423d59;
-  border-right: 1px solid #423d59;
+  border-top: 1px solid
+    ${(props) => (props.theme.displayTheme === "light" ? "#DDD9F3" : "#423d59")};
+  border-left: 1px solid
+    ${(props) => (props.theme.displayTheme === "light" ? "#DDD9F3" : "#423d59")};
+  border-right: 1px solid
+    ${(props) => (props.theme.displayTheme === "light" ? "#DDD9F3" : "#423d59")};
   border-top-left-radius: 15px;
   border-top-right-radius: 15px;
 
-  background: #2f2c3c;
+  background: ${(props) =>
+    props.theme.displayTheme === "light" ? "#F3F0FF" : "#2f2c3c"};
 
   z-index: 2;
   display: flex;
