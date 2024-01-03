75f3a11a97e676a7aea6304206e00eb23609b6b3
nicholas ma <nicholas.maa@gmail.com>
2024-01-02 16:34:29 -0800

    fix: moved nav bar to PageWrapper

--- src/routes/popup/index.tsx
+++ src/routes/popup/index.tsx
@@ -12,7 +12,6 @@ import { isExpired } from "~wallets/auth";
 import { useHistory } from "~utils/hash_router";
 import { trackEvent, EventType, trackPage, PageType } from "~utils/analytics";
 import { findGateway } from "~gateways/wayfinder";
-import { NavigationBar } from "~components/popup/Navigation";
 
 export default function Home() {
   // get if the user has no balance
@@ -64,7 +63,6 @@ export default function Home() {
         </>
       )) || <NoBalance />}
       <AnalyticsConsent />
-      <NavigationBar />
     </>
   );
 }
