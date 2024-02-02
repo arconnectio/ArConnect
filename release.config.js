4f4fc45f885d1b98eda0e75b25a9f8717cce13c8
nicholas ma <nicholas.maa@gmail.com>
2024-02-02 08:47:22 -0800

    chore: adds semantic-release and cws release

---
+++ release.config.js
@@ -0,0 +1,24 @@
+const config = {
+  branches: ["production"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    "@semantic-release/github"
+  ]
+};
+
+module.exports = config;
