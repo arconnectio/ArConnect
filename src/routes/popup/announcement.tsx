9f47d84ce855a95ff8e0df90e4e43eadf2b18439
elehmandevelopment <elehman.development@gmail.com>
2024-02-21 16:58:07 -0700

    feat: add announcement popup

---
+++ src/routes/popup/announcement.tsx
@@ -0,0 +1,70 @@
+import {
+  ButtonV2,
+  Checkbox,
+  Modal,
+  Spacer,
+  Text,
+  useModal
+} from "@arconnect/components";
+import aoGraphic from "url:/assets/ecosystem/ao-arconnect.svg";
+import { Square } from "@untitled-ui/icons-react";
+import styled from "styled-components";
+
+export const AnnouncementPopup = ({ isOpen, setOpen }) => {
+  return (
+    <AnnouncementModal
+      root={document.getElementById("__plasmo")}
+      open={isOpen}
+      setOpen={setOpen}
+      actions={<ButtonV2 onClick={() => setOpen(false)}>Dismiss</ButtonV2>}
+    >
+      <img src={aoGraphic} alt="ao graphic" />
+      <HeaderText noMargin heading>
+        ao testnet is now live!
+      </HeaderText>
+      <Spacer y={1} />
+      <CenterText>
+        Look out for new updates around ao in the future. To learn more visit{" "}
+        <Link>ao.computer</Link>
+      </CenterText>
+      <Spacer y={1} />
+      <Content>
+        <UncheckedBox />
+        <CenterText>Display ao tokens in ArConnect (Beta)</CenterText>
+      </Content>
+    </AnnouncementModal>
+  );
+};
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Link = styled.u`
+  cursor: pointer;
+`;
+
+const UncheckedBox = styled(Square)`
+  color: #a3a3a3;
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+`;
+
+const HeaderText = styled(Text)`
+  font-size: 18px;
+`;
+
+const CenterText = styled(Text)`
+  text-align: center;
+  color: #ffffff;
+  font-size: 12px;
+`;
+
+const AnnouncementModal = styled(Modal)`
+  box-sizing: border-box;
+  width: 300px;
+  height: 364px;
+  padding: 24px 30px;
+`;
--- src/routes/popup/index.tsx
+++ src/routes/popup/index.tsx
@@ -8,6 +8,7 @@ import NoBalance from "~components/popup/home/NoBalance";
 import Balance from "~components/popup/home/Balance";
 import BuyButton from "~components/popup/home/BuyButton";
 import Tokens from "~components/popup/home/Tokens";
+import { AnnouncementPopup } from "./announcement";
 import Arweave from "arweave";
 import { isExpired } from "~wallets/auth";
 import { useHistory } from "~utils/hash_router";
@@ -18,6 +19,7 @@ import styled from "styled-components";
 export default function Home() {
   // get if the user has no balance
   const [noBalance, setNoBalance] = useState(false);
+  const [isOpen, setOpen] = useState(false);
   const [push] = useHistory();
   const [activeAddress] = useStorage<string>({
     key: "active_address",
@@ -52,10 +54,12 @@ export default function Home() {
       }
     };
     checkExpiration();
+    setOpen(true);
   }, []);
 
   return (
     <HomeWrapper>
+      <AnnouncementPopup isOpen={isOpen} setOpen={setOpen} />
       <WalletHeader />
       <Balance />
       {(!noBalance && (
