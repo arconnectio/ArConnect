f5df3ac1bdec8c2206f32c3d8bb7526bbc960467
elehmandevelopment <elehman.development@gmail.com>
2024-02-23 09:24:13 -0700

    refactor: checkbox svgs

--- src/routes/popup/announcement.tsx
+++ src/routes/popup/announcement.tsx
@@ -1,10 +1,20 @@
 import { ButtonV2, Modal, Spacer, Text } from "@arconnect/components";
 import aoGraphic from "url:/assets/ecosystem/ao-arconnect.svg";
 import { CheckSquare, Square } from "@untitled-ui/icons-react";
+import { useStorage } from "@plasmohq/storage/hook";
+import { ExtensionStorage } from "~utils/storage";
 import styled from "styled-components";
 import { useState } from "react";
 
 export const AnnouncementPopup = ({ isOpen, setOpen }) => {
+  const [showAnnouncement, setShowAnnouncement] = useStorage(
+    {
+      key: "showAnnouncement",
+      instance: ExtensionStorage
+    },
+    []
+  );
+
   const [checked, setChecked] = useState(true);
 
   return (
@@ -29,12 +39,36 @@ export const AnnouncementPopup = ({ isOpen, setOpen }) => {
             <Spacer y={1} />
             <CheckContainer>
               {checked ? (
-                <CheckedBox
+                <CheckedSvg
                   onClick={() => setChecked(false)}
-                  stroke={"#8E7BEA"}
-                />
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 16.4L6 12.4L7.4 11L10 13.6L16.6 7L18 8.4L10 16.4Z"
+                    fill="white"
+                  />
+                </CheckedSvg>
               ) : (
-                <UncheckedBox onClick={() => setChecked(true)} />
+                <UncheckedSvg
+                  onClick={() => setChecked(true)}
+                  width="19"
+                  height="18"
+                  viewBox="0 0 19 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="1.5"
+                    y="1"
+                    width="16"
+                    height="16"
+                    rx="1"
+                    stroke="#A3A3A3"
+                    stroke-width="2"
+                  />
+                </UncheckedSvg>
               )}
               <CenterText style={{ marginLeft: "24px" }}>
                 Display AO tokens in ArConnect (Beta)
@@ -102,30 +136,50 @@ const CheckContainer = styled.div`
   gap: 8px;
 `;
 
-const CheckedBox = styled(CheckSquare)`
+const CheckedSvg = styled.svg`
   position: absolute;
-  left: calc(50% - 24px / 2 - 113px);
-  color: #a3a3a3;
-  width: 24px;
-  height: 24px;
+  left: calc(50% - 18px / 2 - 113px);
+  width: 18px;
+  height: 18px;
   cursor: pointer;
   flex: none;
   flex-grow: 0;
-  color: #8e7bea;
-  fill: #8e7bea;
-  stroke: #ffffff;
+  background: #8e7bea;
+  border-radius: 2px;
 `;
 
-const UncheckedBox = styled(Square)`
+const UncheckedSvg = styled.svg`
   position: absolute;
-  left: calc(50% - 24px / 2 - 113px);
-  color: #a3a3a3;
-  width: 24px;
-  height: 24px;
+  left: calc(50% - 18px / 2 - 113px);
+  width: 18px;
+  height: 18px;
   cursor: pointer;
   flex: none;
   flex-grow: 0;
 `;
+
+// const CheckedBox = styled(CheckSquare)`
+//   position: absolute;
+//   left: calc(50% - 24px / 2 - 113px);
+//   width: 24px;
+//   height: 24px;
+//   cursor: pointer;
+//   flex: none;
+//   flex-grow: 0;
+//   color: #FFFFFF;
+//   background: #8E7BEA;
+// `;
+
+// const UncheckedBox = styled(Square)`
+//   position: absolute;
+//   left: calc(50% - 22px / 2 - 113px);
+//   color: #a3a3a3;
+//   width: 22px;
+//   height: 22px;
+//   cursor: pointer;
+//   flex: none;
+//   flex-grow: 0;
+// `;
 
 const HeaderText = styled(Text)`
   font-size: 18px;
