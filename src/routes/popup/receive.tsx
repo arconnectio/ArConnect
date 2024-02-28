ada75fa244fa05e42b87a00bb0188570d1f21aaf
elehmandevelopment <elehman.development@gmail.com>
2024-02-28 11:55:13 -0700

    refactor: update receive page styles and copy

--- src/routes/popup/receive.tsx
+++ src/routes/popup/receive.tsx
@@ -1,16 +1,22 @@
-import { Button, Section, Tooltip } from "@arconnect/components";
-import { CopyIcon, ShareIcon } from "@iconicicons/react";
+import {
+  Button,
+  Section,
+  Tooltip,
+  useToasts,
+  type DisplayTheme
+} from "@arconnect/components";
 import { useStorage } from "@plasmohq/storage/hook";
 import { ExtensionStorage } from "~utils/storage";
+import { CheckIcon, CopyIcon } from "@iconicicons/react";
 import { formatAddress } from "~utils/format";
 import { QRCodeSVG } from "qrcode.react";
 import browser from "webextension-polyfill";
-import Head from "~components/popup/Head";
 import styled from "styled-components";
 import copy from "copy-to-clipboard";
-import { useEffect } from "react";
+import { useEffect, type MouseEventHandler, useState } from "react";
 import { PageType, trackPage } from "~utils/analytics";
 import HeadV2 from "~components/popup/HeadV2";
+import { useTheme } from "~utils/theme";
 
 export default function Receive() {
   // active address
@@ -18,41 +24,59 @@ export default function Receive() {
     key: "active_address",
     instance: ExtensionStorage
   });
+  const [copied, setCopied] = useState(false);
 
   //segment
   useEffect(() => {
     trackPage(PageType.RECEIVE);
   }, []);
 
+  const theme = useTheme();
+  const { setToast } = useToasts();
+
+  const copyAddress: MouseEventHandler = (e) => {
+    e.stopPropagation();
+    copy(activeAddress);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1000);
+    setToast({
+      type: "success",
+      duration: 2000,
+      content: `${formatAddress(activeAddress, 3)} ${browser.i18n.getMessage(
+        "copied_address_2"
+      )}`
+    });
+  };
+
   return (
     <Wrapper>
       <div>
         <HeadV2 title={browser.i18n.getMessage("receive")} />
+      </div>
+      <Section>
+        <QRCodeWrapper displayTheme={theme}>
+          <QRCode
+            displayTheme={theme}
+            fgColor="#fff"
+            bgColor={theme.displayTheme === "light" ? "#7866D3" : "#8E7BEA"}
+            size={285.84}
+            value={activeAddress ?? ""}
+          />
+        </QRCodeWrapper>
+      </Section>
+      <Section>
         <AddressField>
           {formatAddress(activeAddress ?? "", 6)}
           <Tooltip
             content={browser.i18n.getMessage("copy_address")}
             position="bottom"
           >
-            <CopyButton onClick={() => copy(activeAddress)} />
+            <CopyAction
+              as={copied ? CheckIcon : CopyIcon}
+              onClick={copyAddress}
+            />
           </Tooltip>
         </AddressField>
-      </div>
-      <Section>
-        <QRCodeWrapper>
-          <QRCodeSVG
-            fgColor="#fff"
-            bgColor="transparent"
-            size={256}
-            value={activeAddress ?? ""}
-          />
-        </QRCodeWrapper>
-      </Section>
-      <Section>
-        <Button fullWidth>
-          {browser.i18n.getMessage("share")}
-          <ShareIcon />
-        </Button>
       </Section>
     </Wrapper>
   );
@@ -61,26 +85,22 @@ export default function Receive() {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: space-around;
   height: calc(100vh - 72px);
 `;
 
-const AddressField = styled.div`
+const AddressField = styled(Button)`
   display: flex;
   align-items: center;
-  gap: 0.45rem;
-  font-size: 0.9rem;
+  gap: 5px;
   font-weight: 500;
-  padding: 0.45rem 0.8rem;
-  border-radius: 18px;
   margin: 0 auto;
   background-color: rgb(${(props) => props.theme.theme});
   color: #fff;
-  width: max-content;
 `;
 
-const CopyButton = styled(CopyIcon)`
-  font-size: 0.9rem;
+const CopyAction = styled(CopyIcon)`
+  font-size: 1.25rem;
   width: 1em;
   height: 1em;
   color: #fff;
@@ -88,18 +108,23 @@ const CopyButton = styled(CopyIcon)`
   transition: all 0.23s ease-in-out;
 
   &:hover {
-    opacity: 0.86;
+    opacity: 0.85;
   }
 
   &:active {
-    transform: scale(0.9);
+    transform: scale(0.92);
   }
 `;
 
-const QRCodeWrapper = styled.div`
-  width: max-content;
-  margin: 0 auto;
-  background-color: rgb(${(props) => props.theme.theme});
-  border-radius: 24px;
-  padding: 1.35rem;
+const QRCodeWrapper = styled.div<{ displayTheme: DisplayTheme }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: max-content%;
+  background-color: ${(props) =>
+    props.theme.displayTheme === "light" ? "#7866D3" : "#8E7BEA"};
+  border-radius: 21.44px;
+  padding: 17.15px;
 `;
+
+const QRCode = styled(QRCodeSVG)<{ displayTheme: DisplayTheme }>``;
