51dce5774acd7f993bfd2ad7a986384da82506a3
elehmandevelopment <elehman.development@gmail.com>
2024-03-04 13:07:50 -0700

    style: update connect components to v2

--- src/routes/auth/connect.tsx
+++ src/routes/auth/connect.tsx
@@ -1,8 +1,9 @@
 import {
-  Button,
+  ButtonV2,
   Card,
   Checkbox,
-  Input,
+  InputV2,
+  LabelV2,
   Section,
   Spacer,
   Text,
@@ -194,7 +195,7 @@ export default function Connect() {
             {page === "unlock" && (
               <UnlockWrapper>
                 <Section>
-                  <Label>{browser.i18n.getMessage("wallet")}</Label>
+                  <LabelV2>{browser.i18n.getMessage("wallet")}</LabelV2>
                   <Spacer y={0.4} />
                   <WalletSelectWrapper>
                     <WalletSelect
@@ -218,7 +219,7 @@ export default function Connect() {
                     />
                   </WalletSelectWrapper>
                   <Spacer y={1} />
-                  <Input
+                  <InputV2
                     type="password"
                     placeholder={browser.i18n.getMessage("enter_your_password")}
                     label={browser.i18n.getMessage("password")}
@@ -273,7 +274,7 @@ export default function Connect() {
         </ContentWrapper>
       </div>
       <Section>
-        <Button
+        <ButtonV2
           fullWidth
           onClick={async () => {
             if (page === "unlock") {
@@ -284,11 +285,11 @@ export default function Connect() {
           }}
         >
           {browser.i18n.getMessage(page === "unlock" ? "sign_in" : "connect")}
-        </Button>
+        </ButtonV2>
         <Spacer y={0.75} />
-        <Button fullWidth secondary onClick={cancel}>
+        <ButtonV2 fullWidth secondary onClick={cancel}>
           {browser.i18n.getMessage("cancel")}
-        </Button>
+        </ButtonV2>
       </Section>
     </Wrapper>
   );
@@ -299,10 +300,10 @@ const WalletSelectWrapper = styled.div`
 `;
 
 const SelectIcon = styled(ChevronDownIcon)`
-  font-size: 1.25rem;
-  width: 1em;
-  height: 1em;
-  color: rgb(${(props) => props.theme.theme});
+  font-size: 16px;
+  width: 22px;
+  height: 22px;
+  color: ${(props) => props.theme.primaryTextv2};
   transition: all 0.23s ease-in-out;
 `;
 
@@ -313,25 +314,34 @@ const WalletSelect = styled(Card)<{ open: boolean }>`
   justify-content: space-between;
   cursor: pointer;
   background-color: transparent;
-  padding: 0.75rem 1.25rem;
-  border-radius: 18px;
+  padding: 13.5px 15px;
+  border: 1.5px solid ${(props) => props.theme.inputField};
+  border-radius: 10px;
   transition: all 0.23s ease-in-out;
-
-  ${(props) =>
-    props.open
-      ? `border-color: rgba(${props.theme.theme}, .5); box-shadow: 0 0 0 1px rgba(${props.theme.theme}, .5);`
-      : ""}
 
   ${SelectIcon} {
     transform: ${(props) => (props.open ? "rotate(180deg)" : "rotate(0)")};
   }
+
+  &:focus-within,
+  &: hover {
+    ${(props) => "border: 1.5px solid " + props.theme.primaryTextv2};
+  }
+
+  &:active {
+    border-color: ${(props) => props.theme.inputField};
+    color: rgb(${(props) => props.theme.theme});
+  }
 `;
 
 const Address = styled(Text).attrs({
-  noMargin: true
+  noMargin: true,
+  title: false
 })`
-  font-size: 1.2rem;
-  color: rgb(${(props) => props.theme.theme});
+  font-size: 16px;
+  line-height: 22px;
+  font-weight: 500;
+  color: ${(props) => props.theme.primaryTextv2};
 `;
 
 const ContentWrapper = styled.div`
