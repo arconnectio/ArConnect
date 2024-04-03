28b9bf36e6ad43f9feb26caf19f12270b9e156e4
elehmandevelopment <elehman.development@gmail.com>
2024-04-03 14:24:19 -0600

    feat: add rewards card to home

---
+++ src/components/popup/home/Rewards.tsx
@@ -0,0 +1,100 @@
+import { Section, TooltipV2, type DisplayTheme } from "@arconnect/components";
+import { useBalance } from "~wallets/hooks";
+import styled from "styled-components";
+import { InfoCircle } from "@untitled-ui/icons-react";
+import { Text } from "@arconnect/components";
+
+// fetch ar balance
+// fetch ao vault balance
+// handle emissions rate
+// show status in ui
+// show streak in ui (days allocated)
+
+// Any movement of funds disqualifies from using a vault
+
+export default function Rewards() {
+  return (
+    <Wrapper>
+      <RewardsCard>
+        <TopRowInfo>
+          <RewardsTitle>
+            Rewards
+            <TooltipV2 content={"Info"} position={"top"}>
+              <InfoIcon />
+            </TooltipV2>
+          </RewardsTitle>
+          <Rate>1 ao/day</Rate>
+        </TopRowInfo>
+      </RewardsCard>
+    </Wrapper>
+  );
+}
+
+// Rewards title
+// i icon with tooltipv2
+// linear gradient
+// linear gradient
+
+const Rate = styled(Text).attrs({
+  heading: true
+})`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 21.86px;
+  text-align: center;
+`;
+
+const InfoIcon = styled(InfoCircle)`
+  width: 1rem;
+  height: 1rem;
+  gap: 0px;
+  border: 1.33px 0px 0px 0px;
+  opacity: 0px;
+  margin-top: 1px;
+  cursor: pointer;
+  color: ${(props) => props.theme.secondaryTextv2};
+`;
+
+const RewardsTitle = styled(Text).attrs({
+  heading: true
+})`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 21.86px;
+  text-align: center;
+  gap: 3px;
+`;
+
+const TopRowInfo = styled.div`
+  width: 317.5px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const RewardsCard = styled.div`
+  width: 100%;
+  height: 70px;
+  padding: 15px;
+  gap: 4px;
+  background: conic-gradient(
+    from 177.82deg at 47.07% 100.5%,
+    #fe79b9 0deg,
+    #ad00ff 150deg,
+    #2489ff 360deg
+  );
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  height: 100px;
+  margin: 0px 12px;
+  border-radius: 10px;
+`;
