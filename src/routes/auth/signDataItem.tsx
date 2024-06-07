12af321ad1e9ed649afd4cfc93a985f9f0d6ba6e
Pawan Paudel <pawanpaudel93@gmail.com>
2024-06-07 10:05:24 +0545

    refactor: Update Sign Item popup for AO transfer with more details

--- src/routes/auth/signDataItem.tsx
+++ src/routes/auth/signDataItem.tsx
@@ -2,7 +2,6 @@ import { replyToAuthRequest, useAuthParams, useAuthUtils } from "~utils/auth";
 import {
   ButtonV2,
   InputV2,
-  ListItem,
   Loading,
   Section,
   Spacer,
@@ -10,18 +9,30 @@ import {
   useInput,
   useToasts
 } from "@arconnect/components";
+import {
+  FiatAmount,
+  AmountTitle,
+  Properties,
+  TransactionProperty,
+  PropertyName,
+  PropertyValue,
+  TagValue
+} from "~routes/popup/transaction/[id]";
 import Wrapper from "~components/auth/Wrapper";
 import browser from "webextension-polyfill";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import styled from "styled-components";
 import HeadV2 from "~components/popup/HeadV2";
-import { svgie } from "~utils/svgies";
 import { formatAddress } from "~utils/format";
 import { useStorage } from "@plasmohq/storage/hook";
 import { ExtensionStorage } from "~utils/storage";
 import { checkPassword } from "~wallets/auth";
 import { Quantity, Token } from "ao-tokens";
-import { getUserAvatar } from "~lib/avatar";
+import prettyBytes from "pretty-bytes";
+import { formatFiatBalance } from "~tokens/currency";
+import useSetting from "~settings/hook";
+import { getPrice } from "~lib/coingecko";
+import type { TokenInfo } from "~tokens/aoTokens/ao";
 
 interface Tag {
   name: string;
@@ -41,17 +52,16 @@ export default function SignDataItem() {
     data: DataStructure;
   }>();
 
+  const parentRef = useRef(null);
+  const childRef = useRef(null);
   const [password, setPassword] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [tokenName, setTokenName] = useState<string>("");
-  const [logo, setLogo] = useState<string>("");
   const [amount, setAmount] = useState<Quantity | null>(null);
   const { setToast } = useToasts();
 
-  const recipient =
-    params?.data?.tags.find((tag) => tag.name === "Recipient")?.value || "NA";
   const quantity =
-    params?.data?.tags.find((tag) => tag.name === "Quantity")?.value || "NA";
+    params?.data?.tags.find((tag) => tag.name === "Quantity")?.value || "0";
   const process = params?.data?.target;
 
   const [signatureAllowance] = useStorage(
@@ -62,62 +72,28 @@ export default function SignDataItem() {
     10
   );
 
+  // active address
+  const [activeAddress] = useStorage<string>(
+    {
+      key: "active_address",
+      instance: ExtensionStorage
+    },
+    ""
+  );
+
+  // currency setting
+  const [currency] = useSetting<string>("currency");
+
+  // token price
+  const [price, setPrice] = useState(0);
+
+  // transaction price
+  const fiatPrice = useMemo(() => +(amount || 0) * price, [amount]);
+
   // get auth utils
   const { closeWindow, cancel } = useAuthUtils("signDataItem", params?.authID);
 
   const passwordInput = useInput();
-
-  const svgieAvatar = useMemo(
-    () => svgie(recipient, { asDataURI: true }),
-    [recipient]
-  );
-
-  useEffect(() => {
-    if (signatureAllowance < amount?.toNumber()) {
-      setPassword(true);
-    } else {
-      setPassword(false);
-    }
-  }, [signatureAllowance, amount]);
-
-  // get ao token info
-  useEffect(() => {
-    const fetchTokenInfo = async () => {
-      try {
-        if (process) {
-          setLoading(true);
-          const token = await Token(params.data.target);
-          const logo = await getUserAvatar(token?.info?.Logo || "");
-
-          const tokenAmount = new Quantity(
-            BigInt(quantity),
-            token.info.Denomination
-          );
-          setTokenName(token.info.Name);
-          setLogo(logo);
-          setAmount(tokenAmount);
-          setLoading(false);
-        }
-      } catch (err) {
-        console.log("err", err);
-        setLoading(false);
-      }
-    };
-    fetchTokenInfo();
-  }, [params]);
-
-  // listen for enter to reset
-  useEffect(() => {
-    const listener = async (e: KeyboardEvent) => {
-      if (password) return;
-      if (e.key !== "Enter") return;
-      await sign();
-    };
-
-    window.addEventListener("keydown", listener);
-
-    return () => window.removeEventListener("keydown", listener);
-  }, [params?.authID, password]);
 
   // sign message
   async function sign() {
@@ -140,8 +116,92 @@ export default function SignDataItem() {
     closeWindow();
   }
 
+  useEffect(() => {
+    if (amount === null) return;
+
+    if (signatureAllowance < amount?.toNumber()) {
+      setPassword(true);
+    } else {
+      setPassword(false);
+    }
+  }, [signatureAllowance, amount]);
+
+  useEffect(() => {
+    if (!tokenName) return;
+    getPrice(tokenName, currency)
+      .then((res) => setPrice(res))
+      .catch();
+  }, [currency, tokenName]);
+
+  // get ao token info
+  useEffect(() => {
+    const fetchTokenInfo = async () => {
+      if (!process) return;
+      try {
+        setLoading(true);
+        const token = await Token(params.data.target);
+
+        const tokenAmount = new Quantity(
+          BigInt(quantity),
+          token.info.Denomination
+        );
+        setTokenName(token.info.Name);
+        setAmount(tokenAmount);
+      } catch (err) {
+        // fallback
+        console.log("err", err);
+        try {
+          const aoTokens =
+            (await ExtensionStorage.get<(TokenInfo & { processId: string })[]>(
+              "ao_tokens"
+            )) || [];
+          const aoTokensCache =
+            (await ExtensionStorage.get<(TokenInfo & { processId: string })[]>(
+              "ao_tokens_cache"
+            )) || [];
+          const aoTokensCombined = [...aoTokens, ...aoTokensCache];
+          const token = aoTokensCombined.find(
+            ({ processId }) => params.data.target === processId
+          );
+          if (token) {
+            const tokenAmount = new Quantity(
+              BigInt(quantity),
+              BigInt(token.Denomination)
+            );
+            setTokenName(token.Name);
+            setAmount(tokenAmount);
+          }
+        } catch {}
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchTokenInfo();
+  }, [params]);
+
+  // listen for enter to reset
+  useEffect(() => {
+    const listener = async (e: KeyboardEvent) => {
+      if (password) return;
+      if (e.key !== "Enter") return;
+      await sign();
+    };
+
+    window.addEventListener("keydown", listener);
+
+    return () => window.removeEventListener("keydown", listener);
+  }, [params?.authID, password]);
+
+  useEffect(() => {
+    if (!amount || !parentRef.current || !childRef.current) return;
+
+    const parentWidth = parentRef.current.offsetWidth;
+    const newFontSize = parentWidth * 0.05;
+    childRef.current.style.fontSize = `${newFontSize}px`;
+  }, [amount]);
+
   return (
-    <Wrapper>
+    <Wrapper ref={parentRef}>
       <div>
         <HeadV2
           title={browser.i18n.getMessage("sign_item")}
@@ -155,42 +215,79 @@ export default function SignDataItem() {
               params?.appData.appURL
             )}
           </Text>
-          <div>
-            <ListItem
-              small
-              description={formatAddress(recipient, 10)}
-              title={"Recipient"}
-              img={svgieAvatar}
-              onClick={() =>
-                browser.tabs.create({
-                  url: `https://viewblock.io/arweave/address/${recipient}`
-                })
-              }
-            />
-            {loading ? (
-              <Loading />
-            ) : (
-              <ListItem
-                img={logo}
-                small
-                title={tokenName}
-                description={amount?.toLocaleString()}
-                onClick={() =>
-                  browser.tabs.create({
-                    url: `https://ao_marton.g8way.io/#/process/${process}`
-                  })
-                }
-              />
-            )}
-          </div>
         </Description>
-        <Spacer y={0.75} />
+        {params ? (
+          <Section>
+            <FiatAmount>{formatFiatBalance(fiatPrice, currency)}</FiatAmount>
+            <AmountTitle
+              ref={childRef}
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                marginLeft: "auto",
+                justifyContent: "center",
+                marginBottom: "16px"
+              }}
+            >
+              {(amount || 0).toLocaleString()}
+              {!loading ? (
+                <span>{tokenName}</span>
+              ) : (
+                <Loading style={{ width: "16px", height: "16px" }} />
+              )}
+            </AmountTitle>
+            <Properties>
+              <TransactionProperty>
+                <PropertyName>
+                  {browser.i18n.getMessage("transaction_from")}
+                </PropertyName>
+                <PropertyValue>{formatAddress(activeAddress, 6)}</PropertyValue>
+              </TransactionProperty>
+              {params?.data?.target && (
+                <TransactionProperty>
+                  <PropertyName>
+                    {browser.i18n.getMessage("transaction_to")}
+                  </PropertyName>
+                  <PropertyValue>
+                    {formatAddress(params?.data.target, 6)}
+                  </PropertyValue>
+                </TransactionProperty>
+              )}
+              <TransactionProperty>
+                <PropertyName>
+                  {browser.i18n.getMessage("transaction_fee")}
+                </PropertyName>
+                <PropertyValue>0 AR</PropertyValue>
+              </TransactionProperty>
+              <TransactionProperty>
+                <PropertyName>
+                  {browser.i18n.getMessage("transaction_size")}
+                </PropertyName>
+                <PropertyValue>
+                  {prettyBytes(params?.data.data.length)}
+                </PropertyValue>
+              </TransactionProperty>
+              <Spacer y={0.1} />
+              <PropertyName>
+                {browser.i18n.getMessage("transaction_tags")}
+              </PropertyName>
+              <Spacer y={0.05} />
+              {params?.data.tags.map((tag, i) => (
+                <TransactionProperty key={i}>
+                  <PropertyName>{tag.name}</PropertyName>
+                  <TagValue>{tag.value}</TagValue>
+                </TransactionProperty>
+              ))}
+            </Properties>
+          </Section>
+        ) : (
+          <Loading />
+        )}
       </div>
       <Section>
-        <Spacer y={1.25} />
         {password && (
           <>
-            <PasswordWrapper>
+            <PasswordWrapper style={{ paddingTop: 0 }}>
               <InputV2
                 placeholder="Enter your password"
                 small
