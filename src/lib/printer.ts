d1847a074764ca3c959d82c0c1b6dd9165482b9c
Pawan Paudel <pawanpaudel93@gmail.com>
2024-10-17 23:51:59 +0545

    feat: add arweave upload fallback if turbo fails

--- src/lib/printer.ts
+++ src/lib/printer.ts
@@ -5,6 +5,7 @@ import { createData, ArweaveSigner } from "arbundles";
 import { concatGatewayURL } from "~gateways/utils";
 import { findGateway } from "~gateways/wayfinder";
 import browser from "webextension-polyfill";
+import Arweave from "arweave";
 
 const ARCONNECT_PRINTER_ID = "arconnect-permaweb-printer";
 
@@ -134,27 +135,57 @@ export async function handlePrintRequest(
       { name: "print:timestamp", value: new Date().getTime().toString() }
     ];
 
-    // create data item
-    const dataSigner = new ArweaveSigner(decryptedWallet.keyfile);
-    const dataEntry = createData(
-      new Uint8Array(await data.arrayBuffer()),
-      dataSigner,
-      { tags }
-    );
+    let transactionId: string;
 
-    // sign an upload data
-    await dataEntry.sign(dataSigner);
-    await uploadDataToTurbo(dataEntry, "https://turbo.ardrive.io");
-
-    // this has to be one of FAILED, INVALID_DATA, INVALID_TICKET, OK
-    resultCallback("OK");
-
-    // find a gateway to display the result
+    // find a gateway to upload and display the result
     const gateway = await findGateway({});
+
+    const transactionData = new Uint8Array(await data.arrayBuffer());
+
+    try {
+      // create data item
+      const dataSigner = new ArweaveSigner(decryptedWallet.keyfile);
+      const dataEntry = createData(transactionData, dataSigner, { tags });
+
+      // sign an upload data
+      await dataEntry.sign(dataSigner);
+      await uploadDataToTurbo(dataEntry, "https://turbo.ardrive.io");
+
+      // this has to be one of FAILED, INVALID_DATA, INVALID_TICKET, OK
+      resultCallback("OK");
+
+      transactionId = dataEntry.id;
+    } catch (error) {
+      // sign & post if there is something wrong with turbo
+
+      const arweave = Arweave.init(gateway);
+
+      const transaction = await arweave.createTransaction(
+        { data: transactionData },
+        decryptedWallet.keyfile
+      );
+
+      for (const tag of tags) {
+        transaction.addTag(tag.name, tag.value);
+      }
+
+      // sign and upload
+      await arweave.transactions.sign(transaction, decryptedWallet.keyfile);
+      const uploader = await arweave.transactions.getUploader(transaction);
+
+      while (!uploader.isComplete) {
+        await uploader.uploadChunk();
+      }
+
+      // this has to be one of FAILED, INVALID_DATA, INVALID_TICKET, OK
+      resultCallback("OK");
+
+      transactionId = transaction.id;
+    }
 
     // open in new tab
     await chrome.tabs.create({
-      url: `${concatGatewayURL(gateway)}/${dataEntry.id}`
+      url: `${concatGatewayURL(gateway)}/${transactionId}`
     });
   } catch (e) {
     console.log("Printing failed:\n", e);
